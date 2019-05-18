698403076a86905f73bb6ebb7de6ef10f3a3f0b8
klema <vklimen@gmail.com>
2019-05-18 14:42:46 +0300

    Minor updates

--- src/array/MoveZeroes_283/Main.java
+++ src/array/MoveZeroes_283/Main.java
@@ -3,6 +3,9 @@ package array.MoveZeroes_283;
 import java.util.Arrays;
 
 /**
+ * 283. Move Zeroes
+ * https://leetcode.com/problems/move-zeroes/
+ *
  * Given an array nums, write a function to move all 0's to the end of it while maintaining
  * the relative order of the non-zero elements.
  *
--- src/string/RomanToInteger_13/Main.java
+++ src/string/RomanToInteger_13/Main.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 13. Roman to Integer
+ * https://leetcode.com/problems/roman-to-integer/
+ *
  * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
  *
  * Symbol       Value
