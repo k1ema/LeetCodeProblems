cf5e6f103608b0795f859525be3be974eccc2f17
klema <vklimen@gmail.com>
2019-06-05 10:58:57 +0300

    added solution #101

---
+++ src/tree/SymmetricTree_101/Solution.java
@@ -0,0 +1,76 @@
+package tree.SymmetricTree_101;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import tree.utils.TreeNode;
+
+/**
+ * 101. Symmetric Tree
+ *
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ *
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *
+ *
+ * But the following [1,2,2,null,3,null,3] is not:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ *
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ */
+public class Solution {
+    // recursion
+    // tc O(n), sc O(n) in worst case
+    boolean isSymmetric2(TreeNode root) {
+        return compareTrees(root, root);
+    }
+
+    private boolean compareTrees(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        return node1.val == node2.val
+                && compareTrees(node1.left, node2.right)
+                && compareTrees(node1.right, node2.left);
+    }
+
+    // iteration
+    // tc O(n), sc O(n)
+    boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+            if (node1 == null && node2 == null) {
+                continue;
+            } else if (node1 == null || node2 == null) {
+                return false;
+            } else if (node1.val != node2.val) {
+                return false;
+            }
+            queue.add(node1.left);
+            queue.add(node2.right);
+            queue.add(node1.right);
+            queue.add(node2.left);
+        }
+        return true;
+    }
+}
---
+++ src/tree/SymmetricTree_101/SolutionTest.java
@@ -0,0 +1,20 @@
+package tree.SymmetricTree_101;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import tree.utils.Parser;
+import tree.utils.TreeNode;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        TreeNode tree = Parser.parseData(new Integer[] {1, 2, 2, 3, 4, 4, 3, 5, 6, 7, 8, 8, 7, 6, 5});
+        Assert.assertTrue(s.isSymmetric(tree));
+
+        tree = Parser.parseData(new Integer[] {1,2,2,null,3,null,3});
+        System.out.println(tree);
+        Assert.assertFalse(s.isSymmetric(tree));
+    }
+}
