318717ebd9a8cead7347bbd24a904cc064227903
klema <vklimen@gmail.com>
2019-06-05 23:07:42 +0300

    Added solution #617; updated Parser for tree nodes

---
+++ src/tree/MergeTwoBinaryTrees_617/SolutionTest.java
@@ -0,0 +1,21 @@
+package tree.MergeTwoBinaryTrees_617;
+
+import org.junit.Test;
+
+import tree.utils.Parser;
+import tree.utils.TreeNode;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        TreeNode t1 = Parser.stringToTreeNode("[1, 3, 2, 5, null, null, null]");
+        TreeNode t2 = Parser.stringToTreeNode("[2, 1, 3, null, 4, null, 7]");
+        TreeNode tree = s.mergeTrees(t1, t2);
+        String result = Parser.treeNodeToString(tree);
+        System.out.println(result);
+        assertEquals("[3, 4, 5, 5, 4, null, 7, null, null, null, null, null, null]", result);
+    }
+}
--- src/tree/utils/Parser.java
+++ src/tree/utils/Parser.java
@@ -1,5 +1,8 @@
 package tree.utils;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Parser {
     public static TreeNode parseData(Integer[] data) {
         TreeNode[] nodes = new TreeNode[data.length];
@@ -20,4 +23,72 @@ public class Parser {
 
         return nodes.length > 0 ? nodes[0] : null;
     }
+
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static String treeNodeToString(TreeNode root) {
+        if (root == null) {
+            return "[]";
+        }
+
+        String output = "";
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (node == null) {
+                output += "null, ";
+                continue;
+            }
+
+            output += String.valueOf(node.val) + ", ";
+            nodeQueue.add(node.left);
+            nodeQueue.add(node.right);
+        }
+        return "[" + output.substring(0, output.length() - 2) + "]";
+    }
+
 }
