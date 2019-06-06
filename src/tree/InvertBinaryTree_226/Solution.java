a395fa7b685f1a885cb12ea4fe7436dbf30ef141
klema <vklimen@gmail.com>
2019-06-06 22:35:42 +0300

    Added iterative solution to #226

--- src/tree/InvertBinaryTree_226/Solution.java
+++ src/tree/InvertBinaryTree_226/Solution.java
@@ -1,5 +1,8 @@
 package tree.InvertBinaryTree_226;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import tree.utils.TreeNode;
 
 /**
@@ -24,15 +27,35 @@ import tree.utils.TreeNode;
  */
 public class Solution {
     // tc O(n), sc O(n)
-    TreeNode invertTree(TreeNode root) {
+    // recursion
+    TreeNode invertTree1(TreeNode root) {
         if (root == null) {
             return null;
         }
         TreeNode tmp = root.left;
         root.left = root.right;
         root.right = tmp;
-        root.left = invertTree(root.left);
-        root.right = invertTree(root.right);
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    // tc O(n), sc O(n)
+    // iterative
+    TreeNode invertTree(TreeNode root) {
+        Queue<TreeNode> tree = new LinkedList<>();
+        tree.add(root);
+        while (!tree.isEmpty()) {
+            TreeNode st = tree.poll();
+            if (st == null) {
+                continue;
+            }
+            TreeNode tmp = st.left;
+            st.left =  st.right;
+            st.right = tmp;
+            tree.add(st.left);
+            tree.add(st.right);
+        }
         return root;
     }
 }
