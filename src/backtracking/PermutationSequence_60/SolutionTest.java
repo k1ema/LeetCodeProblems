dc8ead739a196dcaafd0b7af2abc05ff2978e32e
klema <vklimen@gmail.com>
2019-06-12 10:45:51 +0300

    minor update

--- src/backtracking/PermutationSequence_60/SolutionTest.java
+++ src/backtracking/PermutationSequence_60/SolutionTest.java
@@ -8,9 +8,9 @@ public class SolutionTest {
     @Test
     public void test() {
         Solution s = new Solution();
-//        assertEquals("213", s.getPermutation(3, 3));
+        assertEquals("213", s.getPermutation(3, 3));
         assertEquals("3142", s.getPermutation(4, 14));
-//        String str = s.getPermutation(9, 135401);
-//        System.out.println(str);
+        String str = s.getPermutation(9, 135401);
+        System.out.println(str);
     }
 }
