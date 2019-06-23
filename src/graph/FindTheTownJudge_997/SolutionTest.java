80a4e6e225ccb05b7148460b413330752cae98f0
klema <vklimen@gmail.com>
2019-06-23 23:16:52 +0300

    Added #997

---
+++ src/graph/FindTheTownJudge_997/SolutionTest.java
@@ -0,0 +1,17 @@
+package graph.FindTheTownJudge_997;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(2, s.findJudge(2, new int[][] {{1, 2}}));
+        assertEquals(3, s.findJudge(3, new int[][] {{1, 3}, {2, 3}}));
+        assertEquals(-1, s.findJudge(3, new int[][] {{1, 3}, {2, 3}, {3, 1}}));
+        assertEquals(-1, s.findJudge(3, new int[][] {{1, 2}, {2, 3}}));
+        assertEquals(3, s.findJudge(4, new int[][] {{1, 3}, {1, 4}, {2, 3}, {2, 4}, {4, 3}}));
+    }
+}
