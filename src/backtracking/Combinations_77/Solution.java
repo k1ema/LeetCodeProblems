b0b6fc9e6887f54d41b05b539f243b0363aa548c
klema <vklimen@gmail.com>
2019-07-12 23:58:49 +0300

    Added solution #77

---
+++ src/backtracking/Combinations_77/Solution.java
@@ -0,0 +1,43 @@
+package backtracking.Combinations_77;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 77. Combinations
+ * https://leetcode.com/problems/combinations/
+ *
+ * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+ *
+ * Example:
+ * Input: n = 4, k = 2
+ * Output:
+ * [
+ *   [2,4],
+ *   [3,4],
+ *   [2,3],
+ *   [1,2],
+ *   [1,3],
+ *   [1,4],
+ * ]
+ */
+public class Solution {
+    List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), 1, n, k);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> combs, List<Integer> temp, int start, int n, int k) {
+        if (k == 0) {
+            combs.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            temp.add(i);
+            backtrack(combs, temp, i + 1, n, k - 1);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
---
+++ src/backtracking/Combinations_77/SolutionTest.java
@@ -0,0 +1,32 @@
+package backtracking.Combinations_77;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(Arrays.asList(
+                Arrays.asList(1, 2),
+                Arrays.asList(1, 3),
+                Arrays.asList(1, 4),
+                Arrays.asList(2, 3),
+                Arrays.asList(2, 4),
+                Arrays.asList(3, 4)
+        ), s.combine(4, 2));
+
+        assertEquals(Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(1, 2, 4),
+                Arrays.asList(1, 3, 4),
+                Arrays.asList(2, 3, 4)
+        ), s.combine(4, 3));
+
+        assertEquals(Collections.singletonList(Arrays.asList(1, 2, 3, 4, 5, 6, 7)), s.combine(7, 7));
+    }
+}
