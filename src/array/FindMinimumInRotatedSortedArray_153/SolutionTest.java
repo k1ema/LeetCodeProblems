e5e4a79f0bb841853e8617f72a2186db36136848
klema <vklimen@gmail.com>
2019-07-14 16:37:52 +0300

    Updated solution #153

--- src/array/FindMinimumInRotatedSortedArray_153/SolutionTest.java
+++ src/array/FindMinimumInRotatedSortedArray_153/SolutionTest.java
@@ -12,5 +12,6 @@ public class SolutionTest {
         assertEquals(0, s.findMin(new int[] {4, 5, 6, 7, 0, 1, 2}));
         assertEquals(0, s.findMin(new int[] {0, 1, 2, 4, 5, 6, 7}));
         assertEquals(0, s.findMin(new int[] {7, 0, 1, 2, 4, 5, 6}));
+        assertEquals(1, s.findMin(new int[] {2, 1}));
     }
 }
