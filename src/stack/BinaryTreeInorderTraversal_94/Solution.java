2901e8afd0d6b71260735bfc71141ba46965198d
klema <vklimen@gmail.com>
2019-07-21 00:15:46 +0300

    Added solution #94

---
+++ src/stack/BinaryTreeInorderTraversal_94/Solution.java
@@ -0,0 +1,78 @@
+package stack.BinaryTreeInorderTraversal_94;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+import tree.utils.TreeNode;
+
+/**
+ * 94. Binary Tree Inorder Traversal
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/
+ *
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ *
+ * Example:
+ *
+ * Input: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * Output: [1,3,2]
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+public class Solution {
+    // tc O(n), sc O(n)
+    List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        goLeft(root, stack);
+
+        if (stack.isEmpty()) {
+            goLeft(root.right, stack);
+        }
+
+        while (!stack.isEmpty()) {
+            TreeNode tn = stack.pop();
+            result.add(tn.val);
+
+            goLeft(tn.right, stack);
+        }
+
+        return result;
+    }
+
+    private void goLeft(TreeNode root, Stack<TreeNode> stack) {
+        while (root != null) {
+            stack.add(root);
+            root = root.left;
+        }
+    }
+
+
+    // recursive
+    // tc O(n), sc O(n)
+    List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        dfs(root, result);
+        return result;
+    }
+
+    private void dfs(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        dfs(root.left, list);
+        list.add(root.val);
+        dfs(root.right, list);
+    }
+}
---
+++ src/stack/BinaryTreeInorderTraversal_94/SolutionTest.java
@@ -0,0 +1,19 @@
+package stack.BinaryTreeInorderTraversal_94;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import tree.utils.Parser;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(Arrays.asList(1,3,2), s.inorderTraversal(Parser.stringToTreeNode("[1,null,2,3]")));
+        assertEquals(Arrays.asList(4, 10, 12, 15, 18, 22, 24, 25, 31, 35, 44, 50, 66, 70, 90),
+                s.inorderTraversal(Parser.stringToTreeNode("[25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90]")));
+    }
+}
