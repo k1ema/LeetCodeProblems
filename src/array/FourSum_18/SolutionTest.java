5480bdd1ab74af9ab158c581253946eb663cb1ce
klema <vklimen@gmail.com>
2019-07-27 13:27:55 +0300

    Uncommented tests for #18

--- src/array/FourSum_18/SolutionTest.java
+++ src/array/FourSum_18/SolutionTest.java
@@ -10,22 +10,22 @@ public class SolutionTest {
     @Test
     public void test() {
         Solution s = new Solution();
-//        assertEquals(Arrays.asList(
-//                Arrays.asList(-2, -1, 1, 2),
-//                Arrays.asList(-2, 0, 0, 2),
-//                Arrays.asList(-1, 0, 0, 1)
-//        ), s.fourSum(new int[] {1, 0, -1, 0, -2, 2}, 0));
+        assertEquals(Arrays.asList(
+                Arrays.asList(-2, -1, 1, 2),
+                Arrays.asList(-2, 0, 0, 2),
+                Arrays.asList(-1, 0, 0, 1)
+        ), s.fourSum(new int[] {1, 0, -1, 0, -2, 2}, 0));
 
-//        assertEquals(Arrays.asList(
-//                Arrays.asList(-3, -2, 2, 3),
-//                Arrays.asList(-3, -1, 1, 3),
-//                Arrays.asList(-3, 0, 0, 3),
-//                Arrays.asList(-3, 0, 1, 2),
-//                Arrays.asList(-2, -1, 0, 3),
-//                Arrays.asList(-2, -1, 1, 2),
-//                Arrays.asList(-2, 0, 0, 2),
-//                Arrays.asList(-1, 0, 0, 1)
-//        ), s.fourSum(new int[] {-3, -2, -1, 0, 0, 1, 2, 3}, 0));
+        assertEquals(Arrays.asList(
+                Arrays.asList(-3, -2, 2, 3),
+                Arrays.asList(-3, -1, 1, 3),
+                Arrays.asList(-3, 0, 0, 3),
+                Arrays.asList(-3, 0, 1, 2),
+                Arrays.asList(-2, -1, 0, 3),
+                Arrays.asList(-2, -1, 1, 2),
+                Arrays.asList(-2, 0, 0, 2),
+                Arrays.asList(-1, 0, 0, 1)
+        ), s.fourSum(new int[] {-3, -2, -1, 0, 0, 1, 2, 3}, 0));
 
         assertEquals(Arrays.asList(Arrays.asList(-3, -3, 2, 4)),
                 s.fourSum(new int[] {2, 4, 0, 4, -3, -3}, 0));
