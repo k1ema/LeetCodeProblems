037cb9dae35d3899cad62351ac304265692a5072
klema <vklimen@gmail.com>
2019-07-31 00:19:29 +0300

    Updated #69 (sqrt)

--- src/binarySearch/Sqrt_69/Solution.java
+++ src/binarySearch/Sqrt_69/Solution.java
@@ -15,7 +15,7 @@ package binarySearch.Sqrt_69;
  * Input: 4
  * Output: 2
  *
- *  Example 2:
+ * Example 2:
  * Input: 8
  * Output: 2
  * Explanation: The square root of 8 is 2.82842..., and since
@@ -29,10 +29,10 @@ public class Solution {
     // https://ru.wikipedia.org/wiki/%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC_%D0%BD%D0%B0%D1%85%D0%BE%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F_%D0%BA%D0%BE%D1%80%D0%BD%D1%8F_n-%D0%BD%D0%BE%D0%B9_%D1%81%D1%82%D0%B5%D0%BF%D0%B5%D0%BD%D0%B8
     // http://webhamster.ru/mytetrashare/index/mtb0/1684
     // https://discuss.leetcode.com/topic/24532/3-4-short-lines-integer-newton-every-language
-    int mySqrt1(int x) {
+    int mySqrt(int x) {
         long r = x;
         while (r * r > x) {
-            r = (r + r / x) / 2;
+            r = (r + x / r) / 2;
         }
         return (int) r;
     }
@@ -41,7 +41,7 @@ public class Solution {
     // https://discuss.leetcode.com/topic/19698/my-clean-c-code-8ms
     // https://discuss.leetcode.com/topic/8680/a-binary-search-solution
     // tc O(logn), sc O(1)
-    int mySqrt(int x) {
+    int mySqrt2(int x) {
         if (x < 2) {
             return x;
         }
--- src/binarySearch/Sqrt_69/SolutionTest.java
+++ src/binarySearch/Sqrt_69/SolutionTest.java
@@ -12,5 +12,9 @@ public class SolutionTest {
         assertEquals(2, s.mySqrt(8));
         assertEquals(4, s.mySqrt(16));
         assertEquals(8, s.mySqrt(64));
+        assertEquals(2, s.mySqrt(5));
+        assertEquals(1, s.mySqrt(3));
+        assertEquals(1, s.mySqrt(1));
+        assertEquals(0, s.mySqrt(0));
     }
 }
