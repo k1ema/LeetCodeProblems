577cb7505893add749a75836b086d3373187453b
klema <vklimen@gmail.com>
2019-09-03 00:04:10 +0300

    Added solution #62

---
+++ src/dynamic/UniquePaths_62/Solution.java
@@ -0,0 +1,66 @@
+package dynamic.UniquePaths_62;
+
+/**
+ * 62. Unique Paths
+ * https://leetcode.com/problems/unique-paths/
+ *
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ *
+ * The robot can only move either down or right at any point in time. The robot is trying to reach
+ * the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ *
+ * How many possible unique paths are there?
+ *
+ * Note: m and n will be at most 100.
+ *
+ * Example 1:
+ * Input: m = 3, n = 2
+ * Output: 3
+ *
+ * Explanation:
+ * From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+ * 1. Right -> Right -> Down
+ * 2. Right -> Down -> Right
+ * 3. Down -> Right -> Right
+ *
+ * Example 2:
+ * Input: m = 7, n = 3
+ * Output: 28
+ */
+public class Solution {
+    // tc O(m * n), sc O(n)
+    int uniquePaths(int m, int n) {
+        if (m == 0 || n == 0) return 0;
+
+        int[] cur = new int[n];
+        for (int i = 0; i < n; i++) {
+            cur[i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] += cur[j - 1];
+            }
+        }
+        return cur[n - 1];
+    }
+
+    // tc O(m * n), sc O(m * n)
+    int uniquePaths1(int m, int n) {
+        if (m == 0 || n == 0) return 0;
+        if (m == 1 || n == 1) return 1;
+
+        int[][] ways = new int[m][n];
+        for (int i = 0; i < Math.max(m, n); i++) {
+            if (i < m) ways[i][0] = 1;
+            if (i < n) ways[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                ways[i][j] += ways[i - 1][j] + ways[i][j - 1];
+            }
+        }
+        return ways[m - 1][n - 1];
+    }
+}
---
+++ src/dynamic/UniquePaths_62/SolutionTest.java
@@ -0,0 +1,19 @@
+package dynamic.UniquePaths_62;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(3, s.uniquePaths(3, 2));
+        assertEquals(28, s.uniquePaths(7, 3));
+        assertEquals(1, s.uniquePaths(1, 2));
+        assertEquals(1, s.uniquePaths(1, 1));
+        assertEquals(15, s.uniquePaths(5, 3));
+        assertEquals(193536720, s.uniquePaths(23, 12));
+        assertEquals(1916797311, s.uniquePaths(51, 9));
+    }
+}
