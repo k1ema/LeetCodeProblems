bd0bb6ef3832de6c6d3a02435e1927e19eb6f386
klema <vklimen@gmail.com>
2019-09-10 21:19:07 +0300

    Added solution #695

---
+++ src/array/MaxAreaOfIsland_695/SolutionTest.java
@@ -0,0 +1,26 @@
+package array.MaxAreaOfIsland_695;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(6, s.maxAreaOfIsland(new int[][] {
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+        }));
+
+        assertEquals(0, s.maxAreaOfIsland(new int[][] {{0, 0, 0, 0, 0, 0, 0, 0}}));
+
+        assertEquals(2, s.maxAreaOfIsland(new int[][] {{0, 1, 1, 0, 1, 0, 1, 1}}));
+    }
+}
