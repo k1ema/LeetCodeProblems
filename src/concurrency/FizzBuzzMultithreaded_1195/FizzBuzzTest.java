8d8a249741767fb1aa9da5fdeb957dfc2803ebb9
klema <vklimen@gmail.com>
2019-09-25 20:19:34 +0300

    Added solution #1195 Fizz Buzz Multithreaded

---
+++ src/concurrency/FizzBuzzMultithreaded_1195/FizzBuzzTest.java
@@ -0,0 +1,19 @@
+package concurrency.FizzBuzzMultithreaded_1195;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+    @Test
+    public void test() {
+        // TODO ?
+//        FizzBuzz fb = new FizzBuzz(15);
+//        try {
+//            fb.fizz(() -> System.out.println("fizz"));
+//            fb.buzz(() -> System.out.println("buzz"));
+//            fb.fizzbuzz(() -> System.out.println("fizzbuzz"));
+//            fb.number(System.out::println);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
