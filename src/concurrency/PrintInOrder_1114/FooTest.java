98f760947c404c74c4869d51745f9c8d6ea51873
klema <vklimen@gmail.com>
2019-09-26 23:24:04 +0300

    Added solution #1114 Print in Order

---
+++ src/concurrency/PrintInOrder_1114/FooTest.java
@@ -0,0 +1,86 @@
+package concurrency.PrintInOrder_1114;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FooTest {
+    private Thread t1, t2, t3;
+
+    @BeforeEach
+    void setUp() {
+        Foo2 foo = new Foo2();
+        t1 = new Thread(() -> {
+            try {
+                foo.first(() -> System.out.print("first"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        });
+        t2 = new Thread(() -> {
+            try {
+                foo.second(() -> System.out.print("second"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        });
+        t3 = new Thread(() -> {
+            try {
+                foo.third(() -> System.out.print("third"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        });
+    }
+
+    @Test
+    @DisplayName("1-2-3")
+    void test1() {
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+    @Test
+    @DisplayName("1-3-2")
+    void test2() {
+        t1.start();
+        t3.start();
+        t2.start();
+    }
+
+    @Test
+    @DisplayName("2-1-3")
+    void test3() {
+        t2.start();
+        t1.start();
+        t3.start();
+    }
+
+    @Test
+    @DisplayName("2-3-1")
+    void test4() {
+        t2.start();
+        t3.start();
+        t1.start();
+    }
+
+    @Test
+    @DisplayName("3-1-2")
+    void test5() {
+        t3.start();
+        t1.start();
+        t2.start();
+    }
+
+    @Test
+    @DisplayName("3-2-1")
+    void test6() {
+        t3.start();
+        t2.start();
+        t1.start();
+    }
+}
