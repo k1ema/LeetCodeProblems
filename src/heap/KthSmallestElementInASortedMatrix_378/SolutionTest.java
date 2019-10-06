3c229b8dbfa4e075f685b1ffb04092a0c8aa9158
klema <vklimen@gmail.com>
2019-10-06 18:45:04 +0300

    Added #378 Kth Smallest Element in a Sorted Matrix solution

---
+++ src/heap/KthSmallestElementInASortedMatrix_378/SolutionTest.java
@@ -0,0 +1,15 @@
+package heap.KthSmallestElementInASortedMatrix_378;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+//        assertEquals(13, s.kthSmallest(new int[][] {{1, 5, 9}, {10, 11, 13}, {12, 13, 15}}, 8));
+//        assertEquals(9, s.kthSmallest(new int[][] {{1, 5, 9}, {2, 6, 10}, {3, 7, 11}}, 7));
+        assertEquals(9, s.kthSmallest(new int[][] {{1, 5, 9}, {2, 6, 15}, {19, 20, 27}}, 5));
+    }
+}
