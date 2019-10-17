cda94472573b756b7f49f7644779d81770902e51
klema <vklimen@gmail.com>
2019-10-17 20:42:52 +0300

    Added #91 Decode Ways solution

---
+++ src/dynamic/DecodeWays_91/SolutionTest.java
@@ -0,0 +1,17 @@
+package dynamic.DecodeWays_91;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+//        assertEquals(2, s.numDecodings("12"));
+//        assertEquals(3, s.numDecodings("123"));
+//        assertEquals(3, s.numDecodings("226"));
+//        assertEquals(5, s.numDecodings("1213"));
+        assertEquals(2, s.numDecodings("012"));
+    }
+}
