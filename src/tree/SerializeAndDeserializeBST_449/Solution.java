3fde8304cb6abe31b1dc12d34c60f1238b7fde9b
klema <vklimen@gmail.com>
2020-02-16 12:42:29 +0200

    Added solution for same problems  ##449, 297

---
+++ src/tree/SerializeAndDeserializeBST_449/Solution.java
@@ -0,0 +1,73 @@
+package tree.SerializeAndDeserializeBST_449;
+
+import tree.utils.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 449. Serialize and Deserialize BST
+ * https://leetcode.com/problems/serialize-and-deserialize-bst/
+ *
+ * Serialization is the process of converting a data structure or object into a sequence of bits so that
+ * it can be stored in a file or memory buffer, or transmitted across a network connection link to be
+ * reconstructed later in the same or another computer environment.
+ *
+ * Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how
+ * your serialization/deserialization algorithm should work. You just need to ensure that a binary search
+ * tree can be serialized to a string and this string can be deserialized to the original tree structure.
+ *
+ * The encoded string should be as compact as possible.
+ *
+ * Note: Do not use class member/global/static variables to store states. Your serialize and deserialize
+ * algorithms should be stateless.
+ */
+public class Solution {
+    // tc O(n), sc O(n)
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "";
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null) {
+                sb.append(node.val);
+                queue.add(node.left);
+                queue.add(node.right);
+            } else {
+                sb.append("-");
+            }
+            sb.append(",");
+        }
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data.isEmpty()) return null;
+        int i = 0;
+        String[] ss = data.split(",");
+        Queue<TreeNode> q = new LinkedList<>();
+        TreeNode root = new TreeNode(Integer.parseInt(ss[i++]));
+        q.add(root);
+        while (!q.isEmpty() && i < ss.length) {
+            TreeNode node = q.poll();
+            String val = ss[i++];
+            if (!"-".equals(val)) {
+                node.left = new TreeNode(Integer.parseInt(val));
+                q.add(node.left);
+            }
+            val = ss[i++];
+            if (!"-".equals(val)) {
+                node.right = new TreeNode(Integer.parseInt(val));
+                q.add(node.right);
+            }
+        }
+
+        return root;
+    }
+}
---
+++ src/tree/SerializeAndDeserializeBST_449/SolutionTest.java
@@ -0,0 +1,29 @@
+package tree.SerializeAndDeserializeBST_449;
+
+import org.junit.Test;
+import tree.utils.Parser;
+import tree.utils.TreeNode;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+
+        TreeNode root = Parser.stringToTreeNode("[10, 5, 15, 3, 6, 12, 17]");
+        assertEquals(root, s.deserialize(s.serialize(root)));
+
+        root = Parser.stringToTreeNode("[10, 5, 15, null, 6, 12, 17]");
+        assertEquals(root, s.deserialize(s.serialize(root)));
+
+        root = Parser.stringToTreeNode("[10, null, 15, null, 20]");
+        assertEquals(root, s.deserialize(s.serialize(root)));
+
+        root = Parser.stringToTreeNode("[10, 5, null, 1]");
+        assertEquals(root, s.deserialize(s.serialize(root)));
+
+        root = Parser.stringToTreeNode("[5,3,6,2,4,null,null,1]");
+        assertEquals(root, s.deserialize(s.serialize(root)));
+    }
+}
