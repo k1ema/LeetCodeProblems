a0a1bb1aa130f4990a17e37683cd054e9666545b
klema <vklimen@gmail.com>
2020-03-03 20:05:13 +0200

    Added 560. Subarray Sum Equals K

---
+++ src/array/SubarraySumEqualsK_560/Solution.java
@@ -0,0 +1,56 @@
+package array.SubarraySumEqualsK_560;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 560. Subarray Sum Equals K
+ * https://leetcode.com/problems/subarray-sum-equals-k/
+ *
+ * Given an array of integers and an integer k, you need to find the total number of continuous subarrays
+ * whose sum equals to k.
+ *
+ * Example 1:
+ * Input:nums = [1,1,1], k = 2
+ * Output: 2
+ *
+ * Note:
+ * The length of the array is in range [1, 20,000].
+ * The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+ */
+public class Solution {
+    // tc O(n), sc O(n)
+    // 10 ms, faster than 98.40%; 41.7 MB, less than 5.43%
+    // https://leetcode.com/problems/subarray-sum-equals-k/solution/
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int count = 0, sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.merge(sum, 1, Integer::sum);
+        }
+        return count;
+    }
+
+    // tc O(n^2), sc O(1)
+    // 259 ms, faster than 5.36%; 41.5 MB, less than 5.43%
+    public int subarraySum1(int[] nums, int k) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            int j = i;
+            while (j >= 0) {
+                sum += nums[j];
+                if (sum == k) {
+                    count++;
+                }
+                j--;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/array/SubarraySumEqualsK_560/SolutionTest.java
@@ -0,0 +1,16 @@
+package array.SubarraySumEqualsK_560;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(2, s.subarraySum(new int[] {1, 1, 1}, 2));
+        assertEquals(4, s.subarraySum(new int[] {1, 1, 1, 1, 1}, 2));
+        assertEquals(3, s.subarraySum(new int[] {1, 10, -5, 7, 3, 2, 1}, 5));
+        assertEquals(6, s.subarraySum(new int[] {3, 4, 7, 2, -3, 1, 4, 2, 1}, 7));
+    }
+}
