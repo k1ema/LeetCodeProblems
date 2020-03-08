44bebbe3c100720316b08db3a1932c00ecd19c31
klema <vklimen@gmail.com>
2020-03-08 17:57:05 +0200

    Added 701. Insert into a Binary Search Tree

---
+++ src/tree/InsertIntoABinarySearchTree_701/SolutionTest.java
@@ -0,0 +1,15 @@
+package tree.InsertIntoABinarySearchTree_701;
+
+import org.junit.Test;
+import tree.utils.Parser;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(Parser.stringToTreeNode("[4,2,7,1,3,5]"),
+                s.insertIntoBST(Parser.stringToTreeNode("[4,2,7,1,3]"), 5));
+    }
+}
