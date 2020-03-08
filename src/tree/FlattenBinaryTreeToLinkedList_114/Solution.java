1fd9f5caf8d6199093c697ed47936c8984744578
klema <vklimen@gmail.com>
2020-03-08 19:35:35 +0200

    Added 114. Flatten Binary Tree to Linked List

---
+++ src/tree/FlattenBinaryTreeToLinkedList_114/Solution.java
@@ -0,0 +1,63 @@
+package tree.FlattenBinaryTreeToLinkedList_114;
+
+import tree.utils.TreeNode;
+
+/**
+ * 114. Flatten Binary Tree to Linked List
+ * https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+ *
+ * Given a binary tree, flatten it to a linked list in-place.
+ *
+ * For example, given the following tree:
+ *
+ *     1
+ *    / \
+ *   2   5
+ *  / \   \
+ * 3   4   6
+ * The flattened tree should look like:
+ *
+ * 1
+ *  \
+ *   2
+ *    \
+ *     3
+ *      \
+ *       4
+ *        \
+ *         5
+ *          \
+ *           6
+ */
+public class Solution {
+    // tc O(n), sc O(n)
+    // 1 ms, faster than 30.03%; 40.8 MB, less than 5.45%
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        TreeNode right = root.right;
+        TreeNode left = dfs(root.left, root);
+        dfs(right, left);
+    }
+
+    private TreeNode dfs(TreeNode cur, TreeNode tree) {
+        if (cur == null) return tree;
+        tree.right = new TreeNode(cur.val);
+        tree.left = null;
+        TreeNode left = dfs(cur.left, tree.right);
+        return dfs(cur.right, left);
+    }
+
+    // tc O(n), sc O(n) (because of recursion)
+    // 0 ms, faster than 100.00%; 37.9 MB, less than 58.18%
+    // https://leetcode.com/problems/flatten-binary-tree-to-linked-list/discuss/36977/My-short-post-order-traversal-Java-solution-for-share
+    // visit nodes starting from the right side, e.g 6-5-4-3-2-1
+    TreeNode prev;
+    public void flatten1(TreeNode root) {
+        if (root == null) return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+}
---
+++ src/tree/FlattenBinaryTreeToLinkedList_114/SolutionTest.java
@@ -0,0 +1,17 @@
+package tree.FlattenBinaryTreeToLinkedList_114;
+
+import org.junit.Test;
+import tree.utils.Parser;
+import tree.utils.TreeNode;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        TreeNode tree = Parser.stringToTreeNode("[1,2,5,3,4,null,6]");
+        s.flatten(tree);
+        assertEquals(Parser.stringToTreeNode("[1,null,2,null,3,null,4,null,5,null,6]"), tree);
+    }
+}
