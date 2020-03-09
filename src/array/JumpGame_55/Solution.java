402547c40b851f04a403209946943abeb294630d
klema <vklimen@gmail.com>
2020-03-09 14:20:29 +0200

    Added 55. Jump Game

---
+++ src/array/JumpGame_55/Solution.java
@@ -0,0 +1,67 @@
+package array.JumpGame_55;
+
+/**
+ * 55. Jump Game
+ * https://leetcode.com/problems/jump-game/
+ *
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ *
+ * Each element in the array represents your maximum jump length at that position.
+ *
+ * Determine if you are able to reach the last index.
+ *
+ * Example 1:
+ * Input: [2,3,1,1,4]
+ * Output: true
+ * Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ *
+ * Example 2:
+ * Input: [3,2,1,0,4]
+ * Output: false
+ * Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which
+ * makes it impossible to reach the last index.
+ */
+public class Solution {
+    // tc O(n^2) in worst case ([4,0,0,0,0]); sc O(1)
+    // 1 ms, faster than 98.20%; 41.1 MB, less than 49.57%
+    // The idea is to find all zero's and for each starting from that index to 0 check if we can jump over it.
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
+        int i = 0;
+        while (i < nums.length - 1) {
+            while (nums[i] != 0 && i < nums.length - 1) i++;
+            if (i == nums.length - 1) break;
+            int j = i - 1;
+            while (j >= 0) {
+                if (nums[j] > i - j) break;
+                j--;
+            }
+            if (j < 0) return false;
+            i++;
+        }
+        return true;
+    }
+
+    // tc O(n), sc O(1)
+    // https://leetcode.com/problems/jump-game/discuss/20932/6-line-java-solution-in-on
+    public boolean canJump1(int[] nums) {
+        int reachable = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > reachable) return false;
+            reachable = Math.max(reachable, i + nums[i]);
+        }
+        return true;
+    }
+
+    // tc O(n), sc O(1)
+    // 1 ms, faster than 98.20%; 41.5 MB, less than 29.06%
+    public boolean canJump2(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+}
---
+++ src/array/JumpGame_55/SolutionTest.java
@@ -0,0 +1,23 @@
+package array.JumpGame_55;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertTrue(s.canJump(new int[] {0}));
+        assertTrue(s.canJump(new int[] {1, 0}));
+        assertTrue(s.canJump(new int[] {1, 0}));
+        assertFalse(s.canJump(new int[] {0, 1}));
+        assertTrue(s.canJump(new int[] {2, 3, 1, 1, 4}));
+        assertFalse(s.canJump(new int[] {3, 2, 1, 0, 4}));
+        assertFalse(s.canJump(new int[] {5, 3, 2, 1, 0, 0, 4}));
+        assertTrue(s.canJump(new int[] {6, 3, 2, 1, 0, 0, 4}));
+        assertTrue(s.canJump(new int[] {5, 3, 2, 1, 0, 2, 0, 4}));
+        assertTrue(s.canJump(new int[] {10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}));
+    }
+}
