6014244d0aa591782f8ea98c853df558ef2241d2
klema <vklimen@gmail.com>
2020-03-11 22:20:43 +0200

    Added 122. Best Time to Buy and Sell Stock II

---
+++ src/array/BestTimeToBuyAndSellStock_II_122/SolutionTest.java
@@ -0,0 +1,15 @@
+package array.BestTimeToBuyAndSellStock_II_122;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(7, s.maxProfit(new int[] {7, 1, 5, 3, 6, 4}));
+        assertEquals(4, s.maxProfit(new int[] {1, 2, 3, 4, 5}));
+        assertEquals(0, s.maxProfit(new int[] {7, 6, 4, 3, 1}));
+    }
+}
