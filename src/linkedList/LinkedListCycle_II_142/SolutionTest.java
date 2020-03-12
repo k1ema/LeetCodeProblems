93f5f1cd0a04c872811e7c029f1033f590a390e2
klema <vklimen@gmail.com>
2020-03-13 00:17:22 +0200

    Updated 142. Linked List Cycle II

--- src/linkedList/LinkedListCycle_II_142/SolutionTest.java
+++ src/linkedList/LinkedListCycle_II_142/SolutionTest.java
@@ -1,14 +1,26 @@
 package linkedList.LinkedListCycle_II_142;
 
+import linkedList.utils.ListNode;
 import org.junit.Test;
 
-import static linkedList.utils.Parser.stringToListNode;
 import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
     @Test
     public void test() {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = b;
+
         Solution s = new Solution();
-        assertEquals(2, s.detectCycle(stringToListNode("[3,2,0,-4,2,0,-4,2,0,-4]")).val);
+        assertEquals(b, s.detectCycle(a));
     }
 }
