8ed74410c5ad59feede3da57ef4d6aa5a82eb9bb
klema <vklimen@gmail.com>
2020-03-22 12:49:35 +0200

    Minor change in KthLargestElementInAnArray_215

--- src/array/KthLargestElementInAnArray_215/Solution.java
+++ src/array/KthLargestElementInAnArray_215/Solution.java
@@ -25,8 +25,47 @@ public class Solution {
     // A Random object we will use later repeatedly to choose random pivots
     private final static Random rnd = new Random();
 
-    // tc O(n), 1 ms, faster than 99.49%; 36.6 MB, less than 90.67%
+    // tc O(n), 1 ms, faster than 97.27%; 41.1 MB, less than 5.18%
     int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        k = n - k;
+        return findKthLargest(nums, k, 0, n - 1);
+    }
+
+    private int findKthLargest(int[] nums, int k, int lo, int hi) {
+        int pInd = partition(nums, lo, hi);
+        if (pInd == k) {
+            return nums[pInd];
+        } else if (pInd < k) {
+            lo = pInd + 1;
+        } else {
+            hi = pInd - 1;
+        }
+        return findKthLargest(nums, k, lo, hi);
+    }
+
+    private int partition(int[] nums, int lo, int hi) {
+        int pivotInd = lo + rnd.nextInt(hi - lo + 1);
+        int pivot = nums[pivotInd];
+        swap(nums, lo, pivotInd);
+        int i = lo, j = hi;
+        while (i < j) {
+            while (i <= j && nums[i] <= pivot) i++;
+            while (i <= j && nums[j] >= pivot) j--;
+            if (i < j) swap(nums, i, j);
+        }
+        swap(nums, lo, j);
+        return j;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    // tc O(n), 1 ms, faster than 99.49%; 36.6 MB, less than 90.67%
+    int findKthLargest2(int[] nums, int k) {
         int n = nums.length;
         k = n - k;
         int lo = 0, hi = n - 1;
@@ -41,52 +80,6 @@ public class Solution {
             }
         }
         return nums[k];
-    }
-
-    // tc O(n); 1 ms, faster than 99.49%; 36.5 MB, less than 90.67%
-    int findKthLargest2(int[] nums, int k) {
-        int n = nums.length;
-        k = n - k;
-        return findKthLargest(nums, k, 0, n - 1);
-    }
-
-    private int findKthLargest(int[] nums, int k, int lo, int hi) {
-        int pInd = partition(nums, lo, hi);
-        if (k < pInd) {
-            return findKthLargest(nums, k, lo, pInd - 1);
-        } else if (k > pInd) {
-            return findKthLargest(nums, k, pInd + 1, hi);
-        } else {
-            return nums[k];
-        }
-    }
-
-    private int partition(int[] nums, int lo, int hi) {
-        // Pick a random pivot. Bounds are [lo, hi].
-        int pivotInd = lo + rnd.nextInt(hi - lo + 1);
-        int pivot = nums[pivotInd];
-        swap(nums, pivotInd, lo);
-
-        int i = lo, j = hi;
-        while (i < j) {
-            while (i <= j && nums[i] <= pivot) {
-                i++;
-            }
-            while (i <= j && nums[j] >= pivot) {
-                j--;
-            }
-            if (i < j) {
-                swap(nums, i, j);
-            }
-        }
-        swap(nums, lo, j);
-        return j;
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
     }
 
     // good explanation: https://github.com/bephrem1/backtobackswe/blob/master/Sorting%2C%20Searching%2C%20%26%20Heaps/kthLargestElement.java
--- src/array/KthLargestElementInAnArray_215/SolutionTest.java
+++ src/array/KthLargestElementInAnArray_215/SolutionTest.java
@@ -11,5 +11,7 @@ public class SolutionTest {
         assertEquals(5, s.findKthLargest(new int[] {3, 2, 1, 5, 6, 4}, 2));
         assertEquals(4, s.findKthLargest(new int[] {3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
         assertEquals(-1, s.findKthLargest(new int[] {-1, -1}, 2));
+        assertEquals(2, s.findKthLargest(new int[] {2, 1}, 1));
+        assertEquals(1, s.findKthLargest(new int[] {2, 1}, 2));
     }
 }
