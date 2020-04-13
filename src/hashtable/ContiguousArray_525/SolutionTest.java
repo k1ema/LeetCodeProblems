70bd00280c7ebe102b434ca9ac0925a642e2bdf6
klema <vklimen@gmail.com>
2020-04-13 11:55:45 +0300

    Added 525. Contiguous Array

---
+++ src/hashtable/ContiguousArray_525/SolutionTest.java
@@ -0,0 +1,15 @@
+package hashtable.ContiguousArray_525;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(2, s.findMaxLength(new int[] {0, 1}));
+        assertEquals(2, s.findMaxLength(new int[] {0, 1, 0}));
+        assertEquals(6, s.findMaxLength(new int[] {1, 0, 1, 1, 0, 1, 0, 1}));
+    }
+}
