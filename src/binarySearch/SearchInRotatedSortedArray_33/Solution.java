bdf9587bb34b0c76bebe36535bbfc0be517f7dd0
klema <vklimen@gmail.com>
2020-04-20 12:07:45 +0300

    Updated 33. Search in Rotated Sorted Array

--- src/binarySearch/SearchInRotatedSortedArray_33/Solution.java
+++ src/binarySearch/SearchInRotatedSortedArray_33/Solution.java
@@ -23,46 +23,48 @@ package binarySearch.SearchInRotatedSortedArray_33;
  * Output: -1
  */
 public class Solution {
+    // find shift (min element index), then use classic binary search considering this shift
     // tc O(logn), sc O(1)
     // 0 ms, faster than 100.00%; 38.1 MB, less than 47.17%
-    public int search1(int[] nums, int target) {
-        int n;
-        if (nums == null || (n = nums.length) == 0) return -1;
-
-        int lo = 0, hi = n - 1;
-        if (nums[n -1] < nums[0]) {
-            while (lo < hi) {
-                int mid = lo + (hi - lo) / 2;
-                if (nums[mid] > nums[mid + 1]) {
-                    lo = mid + 1;
-                    break;
-                } else if (nums[mid] < nums[lo]) {
-                    hi = mid;
-                } else {
-                    lo = mid + 1;
-                }
-            }
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int minInd = 0;
+        if (nums[0]> nums[n - 1]) {
+            minInd = findMinIndex(nums);
         }
-        int ind = lo;
-        hi = ind + n - 1;
+        int lo = minInd, hi = n - 1 + minInd;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            if (target < nums[mid % n]) {
-                hi = mid - 1;
-            } else if (target > nums[mid % n]) {
+            if (nums[mid % n] < target) {
                 lo = mid + 1;
+            } else if (nums[mid % n] > target) {
+                hi = mid - 1;
             } else {
                 return mid % n;
             }
         }
-
         return -1;
+    }
+
+    private int findMinIndex(int[] nums) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (mid + 1 < nums.length && nums[mid + 1] < nums[mid]) {
+                return mid + 1;
+            } else if (nums[mid] > nums[lo]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo;
     }
 
     // one pass, https://leetcode.com/problems/search-in-rotated-sorted-array/solution/
     // tc O(logn), sc O(1)
     // 0 ms, faster than 100.00%; 38.2 MB, less than 46.54%
-    public int search(int[] nums, int target) {
+    public int search1(int[] nums, int target) {
         int n;
         if (nums == null || (n = nums.length) == 0) return -1;
 
@@ -73,13 +75,13 @@ public class Solution {
                 return mid;
             } else {
                 if (nums[mid] >= nums[lo]) {
-                    if (target >= nums[lo] && target < nums[mid]) {
+                    if (target < nums[mid] && target >= nums[lo]) {
                         hi = mid - 1;
                     } else {
                         lo = mid + 1;
                     }
                 } else {
-                    if (target <= nums[hi] && target > nums[mid]) {
+                    if (target > nums[mid] && target <= nums[hi]) {
                         lo = mid + 1;
                     } else {
                         hi = mid - 1;
