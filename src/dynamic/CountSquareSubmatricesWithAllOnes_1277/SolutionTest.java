a643a9a254da5206a115dfdba930e1e5ab2b6b98
klema <vklimen@gmail.com>
2020-05-21 22:01:32 +0300

    Added 1277. Count Square Submatrices with All Ones

---
+++ src/dynamic/CountSquareSubmatricesWithAllOnes_1277/SolutionTest.java
@@ -0,0 +1,27 @@
+package dynamic.CountSquareSubmatricesWithAllOnes_1277;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(15, s.countSquares(new int[][] {
+                {0, 1, 1, 1},
+                {1, 1, 1, 1},
+                {0, 1, 1, 1},
+        }));
+        assertEquals(7, s.countSquares(new int[][] {
+                {1, 0, 1},
+                {1, 1, 0},
+                {1, 1, 0},
+        }));
+        assertEquals(12, s.countSquares(new int[][] {
+                {1, 1, 1, 0},
+                {1, 1, 1, 0},
+                {0, 1, 1, 1},
+        }));
+    }
+}
