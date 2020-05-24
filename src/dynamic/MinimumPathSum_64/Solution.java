34003127448e0cf0b6c9ca85a54edcc548253089
klema <vklimen@gmail.com>
2020-05-24 18:58:30 +0300

    Updated 64. Minimum Path Sum

--- src/dynamic/MinimumPathSum_64/Solution.java
+++ src/dynamic/MinimumPathSum_64/Solution.java
@@ -20,9 +20,29 @@ package dynamic.MinimumPathSum_64;
  * Explanation: Because the path 1→3→1→1→1 minimizes the sum.
  */
 public class Solution {
+    // tc O(m * n), sc O(m * n)
+    // 2 ms, faster than 87.91%; 41.8 MB, less than 87.84%
+    int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = grid[i][j];
+                if (i > 0 && j > 0) {
+                    dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
+                } else if (i != 0 || j != 0) {
+                    if (i == 0) dp[i][j] += dp[i][j - 1];
+                    else dp[i][j] += dp[i - 1][j];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
     // tc O(n), sc O(n), n - number of columns
     // 1 ms, faster than 99.48%; 42 MB, less than 87.84%
-    int minPathSum(int[][] grid) {
+    int minPathSum1(int[][] grid) {
         int n;
         if (grid == null || grid.length == 0 || (n = grid[0].length) == 0) return 0;
         int[] dp = new int[n];
