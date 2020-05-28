b062c10019490d951df2c0029dae82b765840a8a
klema <vklimen@gmail.com>
2020-05-28 18:42:34 +0300

    Updated 338. Counting Bits

--- src/dynamic/CountingBits_338/Solution.java
+++ src/dynamic/CountingBits_338/Solution.java
@@ -25,16 +25,38 @@ package dynamic.CountingBits_338;
 public class Solution {
     // tc O(n), sc O(n)
     // 1 ms, faster than 99.74%; 37.6 MB, less than 5.88%
-    //  f[i] = f[i / 2] + i % 2.
+    // f[i] = f[i / 2] + i % 2.
     int[] countBits(int num) {
         int[] f = new int[num + 1];
-        for (int i=1; i<=num; i++) {
+        for (int i = 1; i <= num; i++) {
             f[i] = f[i >> 1] + (i & 1);
         }
         return f;
     }
 
     // tc O(n), sc O(n)
+    public int[] countBits1(int num) {
+        int[] res = new int[num + 1];
+        if (num == 0) return res;
+
+        for (int i = 1; i <= num; i++) {
+            int v = i & (mostSignificantBit(i) - 1);
+            res[i] = res[v] + 1;
+        }
+        return res;
+    }
+
+    private int mostSignificantBit(int n) {
+        if (n == 0) return n;
+        n |= (n >> 1);
+        n |= (n >> 2);
+        n |= (n >> 4);
+        n |= (n >> 8);
+        n |= (n >> 16);
+        return (n + 1) >> 1;
+    }
+
+    // tc O(n * sizeof(int)), sc O(n)
     // 1ms, faster than 99.74%; 38.4 MB
     int[] countBits2(int num) {
         int[] result = new int[num + 1];
@@ -44,24 +66,6 @@ public class Solution {
             while (tmp != 0) {
                 tmp = tmp & (tmp - 1);
                 count++;
-            }
-            result[i] = count;
-        }
-        return result;
-    }
-
-    // tc O(n), sc O(n)
-    // 2ms, faster than 28.97%; 38.6 MB, less than 5.88%
-    int[] countBits1(int num) {
-        int[] result = new int[num + 1];
-        for (int i = 0; i <= num; i++) {
-            int count = 0;
-            int tmp = i;
-            while (tmp != 0) {
-                if ((tmp & 1) == 1) {
-                    count++;
-                }
-                tmp >>>= 1;
             }
             result[i] = count;
         }
