653e017a226ab813433aa0f366d038e5e7e8c705
klema <vklimen@gmail.com>
2020-06-03 16:25:35 +0300

    Updated 88. Merge Sorted Array

--- src/array/MergeSortedArray_88/Solution.java
+++ src/array/MergeSortedArray_88/Solution.java
@@ -19,8 +19,24 @@ package array.MergeSortedArray_88;
  * Output: [1,2,2,3,5,6]
  */
 public class Solution {
+    // tc O(m + n), sc O(1)
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1, p2 = n - 1;
+        int i = nums1.length - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[i--] = nums1[p1--];
+            } else {
+                nums1[i--] = nums2[p2--];
+            }
+        }
+        while (p2 >= 0) {
+            nums1[i--] = nums2[p2--];
+        }
+    }
+
     // tc O(m*n), sc O(1)
-    void merge(int[] nums1, int m, int[] nums2, int n) {
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
         int j = 0;
         for (int i = 0; i < nums1.length; i++) {
             while (j < n && nums2[j] < nums1[i]) {
--- src/array/MergeSortedArray_88/SolutionTest.java
+++ src/array/MergeSortedArray_88/SolutionTest.java
@@ -1,7 +1,5 @@
 package array.MergeSortedArray_88;
 
-import java.util.Arrays;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,17 +9,18 @@ public class SolutionTest {
         Solution s = new Solution();
         int[] nums1 = new int[] {1, 2, 3, 0, 0, 0};
         s.merge(nums1, 3, new int[] {2, 5, 6}, 3);
-        System.out.println(Arrays.toString(nums1));
         Assert.assertArrayEquals(new int[] {1, 2, 2, 3, 5, 6}, nums1);
 
         nums1 = new int[] {5, 6, 7, 0, 0, 0, 0};
         s.merge(nums1, 3, new int[] {1, 2, 3, 4}, 4);
-        System.out.println(Arrays.toString(nums1));
         Assert.assertArrayEquals(new int[] {1, 2, 3, 4, 5, 6, 7}, nums1);
 
         nums1 = new int[] {0, 1, 6, 7, 11, 0, 0, 0, 0};
         s.merge(nums1, 5, new int[] {2, 3, 4, 10}, 4);
-        System.out.println(Arrays.toString(nums1));
         Assert.assertArrayEquals(new int[] {0, 1, 2, 3, 4, 6, 7, 10, 11}, nums1);
+
+        nums1 = new int[] {0};
+        s.merge(nums1, 0, new int[] {1}, 1);
+        Assert.assertArrayEquals(new int[] {1}, nums1);
     }
 }
