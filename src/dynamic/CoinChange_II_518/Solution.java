b3f5cc31fb5b9dc881f52f97392d9242916b0e88
klema <vklimen@gmail.com>
2020-06-07 10:58:04 +0300

    Updated coin change 2

--- src/dynamic/CoinChange_II_518/Solution.java
+++ src/dynamic/CoinChange_II_518/Solution.java
@@ -44,13 +44,13 @@ public class Solution {
     */
 
     // tc O(m * n), sc O(m), where m = amount, n = coins.length
-    // 3 ms, faster than 49.82%; 34.5 MB, less than 100.00%
+    // 2 ms, faster than 100.00%; 36.7 MB, less than 92.02%
     int change(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
         dp[0] = 1;
         for (int i = 0; i < coins.length; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (j >= coins[i]) dp[j] += dp[j - coins[i]];
+            for (int j = coins[i]; j <= amount; j++) {
+                dp[j] += dp[j - coins[i]];
             }
         }
         return dp[amount];
