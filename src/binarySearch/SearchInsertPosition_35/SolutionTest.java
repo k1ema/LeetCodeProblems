9feb1c3130a653d08fd2f0a4dc73d2e328a93444
klema <vklimen@gmail.com>
2020-06-10 10:20:16 +0300

    Added 35. Search Insert Position

---
+++ src/binarySearch/SearchInsertPosition_35/SolutionTest.java
@@ -0,0 +1,17 @@
+package binarySearch.SearchInsertPosition_35;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(2, s.searchInsert(new int[] {1, 3, 5, 6}, 5));
+        assertEquals(1, s.searchInsert(new int[] {1, 3, 5, 6}, 2));
+        assertEquals(4, s.searchInsert(new int[] {1, 3, 5, 6}, 7));
+        assertEquals(0, s.searchInsert(new int[] {1, 3, 5, 6}, 0));
+        assertEquals(3, s.searchInsert(new int[] {1, 3, 5, 8}, 7));
+    }
+}
