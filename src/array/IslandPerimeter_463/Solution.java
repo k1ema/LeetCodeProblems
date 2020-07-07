18c2004eb3cc3a572fb8f8d99450efdf35f04292
klema <vklimen@gmail.com>
2020-07-07 10:28:51 +0300

    Added 463. Island Perimeter

---
+++ src/array/IslandPerimeter_463/Solution.java
@@ -0,0 +1,55 @@
+package array.IslandPerimeter_463;
+
+/**
+ * 463. Island Perimeter
+ * https://leetcode.com/problems/island-perimeter/
+ *
+ * You are given a map in form of a two-dimensional integer grid where 1 represents land
+ * and 0 represents water.
+ *
+ * Grid cells are connected horizontally/vertically (not diagonally). The grid is completely
+ * surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+ *
+ * The island doesn't have "lakes" (water inside that isn't connected to the water around
+ * the island). One cell is a square with side length 1. The grid is rectangular, width
+ * and height don't exceed 100. Determine the perimeter of the island.
+ *
+ * Example:
+ * Input:
+ * [[0,1,0,0],
+ *  [1,1,1,0],
+ *  [0,1,0,0],
+ *  [1,1,0,0]]
+ * Output: 16
+ */
+public class Solution {
+    // tc O(mn), sc O(1)
+    public int islandPerimeter(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0) continue;
+                if (i == 0 || grid[i - 1][j] == 0) res++;
+                if (i == grid.length - 1 || grid[i + 1][j] == 0) res++;
+                if (j == 0 || grid[i][j - 1] == 0) res++;
+                if (j == grid[i].length - 1 || grid[i][j + 1] == 0) res++;
+            }
+        }
+        return res;
+    }
+
+    public int islandPerimeter1(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0) continue;
+                res += 4;
+                if (i > 0 && grid[i - 1][j] == 1) res -= 2;
+                if (j > 0 && grid[i][j - 1] == 1) res -= 2;
+            }
+        }
+        return res;
+    }
+}
---
+++ src/array/IslandPerimeter_463/SolutionTest.java
@@ -0,0 +1,13 @@
+package array.IslandPerimeter_463;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(16, s.islandPerimeter(new int[][] {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}}));
+    }
+}
