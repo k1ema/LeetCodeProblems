383bda17fe6227709f43ead3f539c23dd090b581
klema <vklimen@gmail.com>
2020-07-08 12:15:16 +0300

    Updated 15. 3Sum

--- src/array/ThreeSum_15/Solution.java
+++ src/array/ThreeSum_15/Solution.java
@@ -25,24 +25,50 @@ import java.util.List;
 public class Solution {
     // tc O(n^2), sc O(n)
     List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i == 0 || nums[i] != nums[i - 1]) {
-                int lo = i + 1;
-                int hi = nums.length - 1;
-                while (lo < hi) {
-                    int sum = nums[i] + nums[lo] + nums[hi];
-                    if (sum == 0) {
-                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
-                        while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
-                        while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
-                        lo++; hi--;
-                    } else if (sum < 0) {
-                        lo++;
-                    } else {
-                        hi--;
-                    }
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            twoSum(nums, i, res);
+        }
+        return res;
+    }
+
+    private void twoSum(int[] nums, int i, List<List<Integer>> res) {
+        int l = i + 1, r = nums.length - 1;
+        while (l < r) {
+            int sum = nums[i] + nums[l] + nums[r];
+            if (sum < 0 || nums[l] > i + 1 && nums[l] == nums[l - 1]) {
+                l++;
+            } else if (sum > 0 || r < nums.length - 1 && nums[r] == nums[r + 1]) {
+                r--;
+            } else {
+                res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                l++; r--;
+            }
+        }
+    }
+
+    // tc O(n^2), sc O(n)
+    List<List<Integer>> threeSum1(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) return res;
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l + 1] == nums[l]) l++;
+                    while (l < r && nums[r - 1] == nums[r]) r--;
+                    l++; r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    r--;
                 }
             }
         }
