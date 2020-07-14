aef6378fc632c807eef91baec49462527fc32409
klema <vklimen@gmail.com>
2020-07-14 12:37:12 +0300

    Added 1344. Angle Between Hands of a Clock

---
+++ src/math/AngleBetweenHandsOfAClock_1344/Solution.java
@@ -0,0 +1,41 @@
+package math.AngleBetweenHandsOfAClock_1344;
+
+/**
+ * 1344. Angle Between Hands of a Clock
+ * https://leetcode.com/problems/angle-between-hands-of-a-clock/
+ *
+ * Given two numbers, hour and minutes. Return the smaller angle (in degrees) formed between the hour and the minute hand.
+ *
+ * Example 1:
+ * Input: hour = 12, minutes = 30
+ * Output: 165
+ *
+ * Example 2:
+ * Input: hour = 3, minutes = 30
+ * Output: 75
+ *
+ * Example 3:
+ * Input: hour = 3, minutes = 15
+ * Output: 7.5
+ *
+ * Example 4:
+ * Input: hour = 4, minutes = 50
+ * Output: 155
+ *
+ * Example 5:
+ * Input: hour = 12, minutes = 0
+ * Output: 0
+ *
+ * Constraints:
+ * 1 <= hour <= 12
+ * 0 <= minutes <= 59
+ * Answers within 10^-5 of the actual value will be accepted as correct.
+ */
+public class Solution {
+    public double angleClock(int hour, int minutes) {
+        double hourAngle = 30 * (hour + minutes / 60.0);
+        double minAngle = 360 * (minutes / 60.0);
+        double res = Math.abs(hourAngle - minAngle);
+        return Math.min(res, 360 - res);
+    }
+}
---
+++ src/math/AngleBetweenHandsOfAClock_1344/SolutionTest.java
@@ -0,0 +1,17 @@
+package math.AngleBetweenHandsOfAClock_1344;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(165, s.angleClock(12, 30), 0.00001);
+        assertEquals(75, s.angleClock(3, 30), 0.00001);
+        assertEquals(7.5, s.angleClock(3, 15), 0.00001);
+        assertEquals(155, s.angleClock(4, 50), 0.00001);
+        assertEquals(0, s.angleClock(12, 0), 0.00001);
+    }
+}
