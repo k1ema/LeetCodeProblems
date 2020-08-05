c5fa040f1d644f33fb710d36bb1d9869bdc9db10
klema <vklimen@gmail.com>
2020-08-05 21:20:27 +0300

    Updated 140. Word Break II

--- src/backtracking/WordBreak_II_140/Solution.java
+++ src/backtracking/WordBreak_II_140/Solution.java
@@ -44,8 +44,56 @@ import java.util.*;
  * []
  */
 public class Solution {
-    // https://leetcode.com/problems/word-break-ii/discuss/44167/My-concise-JAVA-solution-based-on-memorized-DFS
+    // tc O(n^2 + 2^n + w), sc O(n * 2^n + w), see https://leetcode.com/articles/word-break-ii/
+    // sc O(n * 2^n + w)
+    // 5 ms, faster than 93.63%; 40.2 MB, less than 14.49%
     public List<String> wordBreak(String s, List<String> wordDict) {
+        List<List<String>> solutions = bt(s, new HashSet<>(wordDict), new HashMap<>());
+
+        List<String> res = new ArrayList<>();
+        for (List<String> words : solutions) {
+            StringBuilder sb = new StringBuilder();
+            for (String word : words) {
+                if (sb.length() != 0) sb.append(" ");
+                sb.append(word);
+            }
+            res.add(sb.toString());
+        }
+
+        return res;
+    }
+
+    private List<List<String>> bt(String s, Set<String> wordDict, Map<String, List<List<String>>> memo) {
+        if (s.isEmpty()) {
+            List<List<String>> solutions = new ArrayList<>();
+            solutions.add(new ArrayList<>());
+            return solutions;
+        }
+
+        if (memo.containsKey(s)) {
+            return memo.get(s);
+        } else {
+            List<List<String>> list = new ArrayList<>();
+            memo.put(s, list);
+        }
+
+        for (int i = 1; i <= s.length(); i++) {
+            String word = s.substring(0, i);
+            if (wordDict.contains(word)) {
+                List<List<String>> list = bt(s.substring(i), wordDict, memo);
+                for (List<String> words : list) {
+                    List<String> res = new LinkedList<>(words);
+                    res.add(0, word);
+                    memo.get(s).add(res);
+                }
+            }
+        }
+
+        return memo.get(s);
+    }
+
+    // https://leetcode.com/problems/word-break-ii/discuss/44167/My-concise-JAVA-solution-based-on-memorized-DFS
+    public List<String> wordBreak1(String s, List<String> wordDict) {
         return bt(s, 0, new HashSet<>(wordDict), new HashMap<>());
     }
 
