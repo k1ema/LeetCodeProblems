1c4b0f2862fe41dac0f1ebe61747ff4418b64633
klema <vklimen@gmail.com>
2020-10-14 21:54:39 +0300

    Added 1422. Maximum Score After Splitting a String

---
+++ src/string/MaximumScoreAfterSplittingAString_1422/SolutionTest.java
@@ -0,0 +1,15 @@
+package string.MaximumScoreAfterSplittingAString_1422;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(5, s.maxScore("011101"));
+        assertEquals(5, s.maxScore("00111"));
+        assertEquals(3, s.maxScore("1111"));
+    }
+}
