255a5f73eea3dc5829ff79ce6b2450c0b7b509c7
klema <vklimen@gmail.com>
2020-10-16 11:01:36 +0300

    Added 74. Search a 2D Matrix

---
+++ src/binarySearch/SearchA2DMatrix_74/Solution.java
@@ -0,0 +1,66 @@
+package binarySearch.SearchA2DMatrix_74;
+
+/**
+ * 74. Search a 2D Matrix
+ * https://leetcode.com/problems/search-a-2d-matrix/
+ *
+ * Write an efficient algorithm that searches for a value in an m x n matrix.
+ * This matrix has the following properties:
+ * - Integers in each row are sorted from left to right.
+ * - The first integer of each row is greater than the last integer of the previous row.
+ *
+ * Example 1:
+ * Input: matrix = [
+ *              [1,3,5,7],
+ *              [10,11,16,20],
+ *              [23,30,34,50]], target = 3
+ * Output: true
+ *
+ * Example 2:
+ * Input: matrix = [
+ *              [1,3,5,7],
+ *              [10,11,16,20],
+ *              [23,30,34,50]], target = 13
+ * Output: false
+ *
+ * Example 3:
+ * Input: matrix = [], target = 0
+ * Output: false
+ *
+ * Constraints:
+ * m == matrix.length
+ * n == matrix[i].length
+ * 0 <= m, n <= 100
+ * -104 <= matrix[i][j], target <= 104
+ */
+public class Solution {
+    /*
+        tc O(logm * logn), sc O(1) - 2d binary search
+        1. use bs to fetch proper row
+        2. use bs to find if that row contains target
+    */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) return false;
+        int l = -1, r = matrix.length;
+        while (r - l > 1) {
+            int m = (l + r) >>> 1;
+            if (matrix[m][0] <= target) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        if (l == -1) return false;
+        int row = l;
+        l = -1; r = matrix[0].length;
+        while (r - l > 1) {
+            int m = (l + r) >>> 1;
+            if (matrix[row][m] <= target) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        return l != -1 && matrix[row][l] == target;
+    }
+}
---
+++ src/binarySearch/SearchA2DMatrix_74/SolutionTest.java
@@ -0,0 +1,29 @@
+package binarySearch.SearchA2DMatrix_74;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertTrue(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 1));
+        assertTrue(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 3));
+        assertTrue(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 7));
+        assertTrue(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 10));
+        assertTrue(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 16));
+        assertTrue(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 20));
+        assertTrue(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 23));
+        assertTrue(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 50));
+        assertFalse(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 0));
+        assertFalse(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 4));
+        assertFalse(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 8));
+        assertFalse(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 12));
+        assertFalse(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 21));
+        assertFalse(s.searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 55));
+        assertFalse(s.searchMatrix(new int[][] {}, 55));
+        assertFalse(s.searchMatrix(new int[][] {{}}, 55));
+    }
+}
