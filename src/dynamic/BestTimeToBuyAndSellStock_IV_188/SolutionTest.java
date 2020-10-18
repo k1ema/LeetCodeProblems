67fc732224fc4ae26678611b061a9dcc22e6933a
klema <vklimen@gmail.com>
2020-10-18 19:24:47 +0300

    Added 188. Best Time to Buy and Sell Stock IV

---
+++ src/dynamic/BestTimeToBuyAndSellStock_IV_188/SolutionTest.java
@@ -0,0 +1,16 @@
+package dynamic.BestTimeToBuyAndSellStock_IV_188;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(2, s.maxProfit(2, new int[] {2, 4, 1}));
+        assertEquals(7, s.maxProfit(2, new int[] {3, 2, 6, 5, 0, 3}));
+        assertEquals(11, s.maxProfit(2, new int[] {0, 4, 2, 7, 2, 6}));
+        assertEquals(0, s.maxProfit(0, new int[] {1, 3}));
+    }
+}
