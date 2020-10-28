ce563f1fcf389045231db5c90f7289249be462f0
klema <vklimen@gmail.com>
2020-10-28 09:17:41 +0200

    Added 428. Serialize and Deserialize N-ary Tree

---
+++ src/tree/SerializeAndDeserializeN_aryTree_428/SolutionTest.java
@@ -0,0 +1,13 @@
+package tree.SerializeAndDeserializeN_aryTree_428;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+//        assertEquals(, s.deserialize(s.serialize()));
+    }
+}
