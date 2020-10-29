d558135a639b57d12370318a5062fa7df1d631fb
klema <vklimen@gmail.com>
2020-10-29 22:12:19 +0200

    Added 849. Maximize Distance to Closest Person

---
+++ src/array/MaximizeDistanceToClosestPerson_849/SolutionTest.java
@@ -0,0 +1,15 @@
+package array.MaximizeDistanceToClosestPerson_849;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(2, s.maxDistToClosest(new int[] {1, 0, 0, 0, 1, 0, 1}));
+        assertEquals(3, s.maxDistToClosest(new int[] {1, 0, 0, 0}));
+        assertEquals(1, s.maxDistToClosest(new int[] {0, 1}));
+    }
+}
