77cbaedad94b44e7dd0450e40522e1234359be28
klema <vklimen@gmail.com>
2020-11-01 16:23:39 +0200

    Updated 252. Meeting Rooms

--- src/sorting/MeetingRooms_252/Solution.java
+++ src/sorting/MeetingRooms_252/Solution.java
@@ -22,15 +22,9 @@ import java.util.Comparator;
  */
 public class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        if (intervals == null || intervals.length == 0) return true;
-        Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
-        int[] prevInterval = intervals[0];
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
         for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] < prevInterval[1]) {
-                return false;
-            } else {
-                prevInterval = intervals[i];
-            }
+            if (intervals[i][0] < intervals[i-1][1]) return false;
         }
         return true;
     }
