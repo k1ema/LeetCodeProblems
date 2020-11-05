8cb70b674ff99b310442864bb4ed8c85c6e68a8d
klema <vklimen@gmail.com>
2020-11-05 12:41:03 +0200

    Added 1217. Minimum Cost to Move Chips to The Same Position

---
+++ src/math/MinimumCostToMoveChipsToTheSamePosition_1217/SolutionTest.java
@@ -0,0 +1,15 @@
+package math.MinimumCostToMoveChipsToTheSamePosition_1217;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(1, s.minCostToMoveChips(new int[] {1, 2, 3}));
+        assertEquals(2, s.minCostToMoveChips(new int[] {2, 2, 2, 3, 3}));
+        assertEquals(1, s.minCostToMoveChips(new int[] {1, 1000000000}));
+    }
+}
