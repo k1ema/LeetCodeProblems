3530f24f0b9096b36159bd321f926a5156105cc9
klema <vklimen@gmail.com>
2020-11-06 12:37:08 +0200

    Added 1283. Find the Smallest Divisor Given a Threshold

---
+++ src/binarySearch/FindTheSmallestDivisorGivenAThreshold_1283/SolutionTest.java
@@ -0,0 +1,16 @@
+package binarySearch.FindTheSmallestDivisorGivenAThreshold_1283;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(5, s.smallestDivisor(new int[] {1, 2, 5, 9}, 6));
+        assertEquals(3, s.smallestDivisor(new int[] {2, 3, 5, 7, 11}, 11));
+        assertEquals(4, s.smallestDivisor(new int[] {19}, 5));
+        assertEquals(82, s.smallestDivisor(new int[] {1, 2, 5, 9, 10, 123, 737, 15}, 17));
+    }
+}
