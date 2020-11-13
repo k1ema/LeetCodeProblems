0525d63feab1aa78dc3231936b51f267a18cb278
klema <vklimen@gmail.com>
2020-11-13 11:04:22 +0200

    Updated 116. Populating Next Right Pointers in Each Node

--- src/tree/PopulatingNextRightPointersInEachNode_116/Solution.java
+++ src/tree/PopulatingNextRightPointersInEachNode_116/Solution.java
@@ -1,6 +1,7 @@
 package tree.PopulatingNextRightPointersInEachNode_116;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * 116. Populating Next Right Pointers in Each Node
@@ -34,37 +35,14 @@ import java.util.Stack;
  * Recursive approach is fine, implicit stack space does not count as extra space for this problem.
  */
 public class Solution {
-    private Stack<Node> stack = new Stack<>();
-
-    // tc O(n), sc O(logn) - is not eligible because I used stack
-    Node connect1(Node root) {
-        if (root == null) return null;
-
-        if (!stack.isEmpty()) {
-            stack.pop().next = root.left;
-        }
-
-        root.left = connect(root.left);
-        root.right = connect(root.right);
-
-        if (root.left != null) {
-            root.left.next = root.right;
-        }
-        if (root.right != null) {
-            stack.push(root.right);
-        }
-
-        return root;
-    }
-
     // https://leetcode.com/problems/populating-next-right-pointers-in-each-node/discuss/37473/My-recursive-solution(Java)
     // tc O(n), sc O(1)
-    Node connect(Node root) {
+    public Node connect(Node root) {
         dfs(root);
         return root;
     }
 
-    void dfs(Node root) {
+    private void dfs(Node root) {
         if (root == null) return;
 
         if (root.left != null) {
@@ -76,5 +54,30 @@ public class Solution {
 
         dfs(root.left);
         dfs(root.right);
+    }
+
+    /*
+        use bfs with level order traversal,
+        init prev node on each level and set prev.next = cur
+    */
+    // tc O(n), sc O(n)
+    public Node connect1(Node root) {
+        if (root == null) return null;
+        Deque<Node> queue = new ArrayDeque<>();
+        queue.addFirst(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Node prev = null;
+            for (int i = 0; i < size; i++) {
+                Node cur = queue.pollLast();
+                if (prev != null) {
+                    prev.next = cur;
+                }
+                if (cur.left != null) queue.addFirst(cur.left);
+                if (cur.right != null) queue.addFirst(cur.right);
+                prev = cur;
+            }
+        }
+        return root;
     }
 }
