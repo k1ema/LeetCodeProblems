06f556d49986b7ed26ceefc35deeb9cfa59b0eae
klema <vklimen@gmail.com>
2020-11-13 12:38:17 +0200

    Updated 15. 3Sum

--- src/array/ThreeSum_15/Solution.java
+++ src/array/ThreeSum_15/Solution.java
@@ -38,7 +38,7 @@ public class Solution {
         int l = i + 1, r = nums.length - 1;
         while (l < r) {
             int sum = nums[i] + nums[l] + nums[r];
-            if (sum < 0 || nums[l] > i + 1 && nums[l] == nums[l - 1]) {
+            if (sum < 0 || l > i + 1 && nums[l] == nums[l - 1]) {
                 l++;
             } else if (sum > 0 || r < nums.length - 1 && nums[r] == nums[r + 1]) {
                 r--;
