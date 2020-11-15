eccbe5c644f2ec0cb6f73fdf24f758cf9bebf062
klema <vklimen@gmail.com>
2020-11-15 14:50:57 +0200

    Added 938. Range Sum of BST

---
+++ src/tree/RangeSumOfBST_938/SolutionTest.java
@@ -0,0 +1,15 @@
+package tree.RangeSumOfBST_938;
+
+import org.junit.Test;
+import tree.utils.Parser;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(32, s.rangeSumBST(Parser.stringToTreeNode("[10,5,15,3,7,null,18]"), 7, 15));
+        assertEquals(23, s.rangeSumBST(Parser.stringToTreeNode("[10,5,15,3,7,13,18,1,null,6]"), 6, 10));
+    }
+}
