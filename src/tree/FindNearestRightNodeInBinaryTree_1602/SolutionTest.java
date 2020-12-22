c57ad3dabe050cf801f5a3196a2a4147c6d9caa4
klema <vklimen@gmail.com>
2020-12-22 18:45:45 +0200

    Added 1602. Find Nearest Right Node in Binary Tree

---
+++ src/tree/FindNearestRightNodeInBinaryTree_1602/SolutionTest.java
@@ -0,0 +1,19 @@
+package tree.FindNearestRightNodeInBinaryTree_1602;
+
+import org.junit.Test;
+import tree.utils.Parser;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(Parser.stringToTreeNode("[5]"),
+                s.findNearestRightNode(Parser.stringToTreeNode("[1,2,3,null,4,5,6]"), Parser.stringToTreeNode("[4]")));
+        assertEquals(null, s.findNearestRightNode(Parser.stringToTreeNode("[3,null,4,2]"), Parser.stringToTreeNode("[2]")));
+        assertEquals(null, s.findNearestRightNode(Parser.stringToTreeNode("[1]"), Parser.stringToTreeNode("[1]")));
+        assertEquals(Parser.stringToTreeNode("[2,null,1]"),
+                s.findNearestRightNode(Parser.stringToTreeNode("[3,4,2,null,null,null,1]"), Parser.stringToTreeNode("[4]")));
+    }
+}
