47ece7d5df37b2f92788877f2dd58e99cb72c342
klema <vklimen@gmail.com>
2020-12-28 19:43:58 +0200

    Updated 55. Jump Game

--- src/array/JumpGame_55/Solution.java
+++ src/array/JumpGame_55/Solution.java
@@ -22,8 +22,26 @@ package array.JumpGame_55;
  * makes it impossible to reach the last index.
  */
 public class Solution {
+    // my solution
     // tc O(n), sc O(1)
     public boolean canJump(int[] nums) {
+        int zeroes = 0;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] == 0) {
+                zeroes++;
+            } else {
+                if (nums[i] <= zeroes) {
+                    zeroes++;
+                } else {
+                    zeroes = 0;
+                }
+            }
+        }
+        return zeroes == 0;
+    }
+
+    // tc O(n), sc O(1)
+    public boolean canJump1(int[] nums) {
         if (nums == null || nums.length == 0) return false;
         int maxJumps = nums[0];
         for (int i = 1; i < nums.length; i++) {
@@ -36,7 +54,7 @@ public class Solution {
 
     // tc O(n), sc O(1)
     // 1 ms, faster than 98.20%; 41.5 MB, less than 29.06%
-    public boolean canJump3(int[] nums) {
+    public boolean canJump2(int[] nums) {
         int lastPos = nums.length - 1;
         for (int i = nums.length - 1; i >= 0; i--) {
             if (i + nums[i] >= lastPos) {
@@ -48,7 +66,7 @@ public class Solution {
 
     // tc O(n), sc O(1)
     // https://leetcode.com/problems/jump-game/discuss/20932/6-line-java-solution-in-on
-    public boolean canJump1(int[] nums) {
+    public boolean canJump3(int[] nums) {
         int reachable = 0;
         for (int i = 0; i < nums.length; i++) {
             if (i > reachable) return false;
@@ -60,7 +78,7 @@ public class Solution {
     // tc O(n^2) in worst case ([4,0,0,0,0]); sc O(1)
     // 1 ms, faster than 98.20%; 41.1 MB, less than 49.57%
     // The idea is to find all zero's and for each starting from that index to 0 check if we can jump over it.
-    public boolean canJump2(int[] nums) {
+    public boolean canJump4(int[] nums) {
         if (nums == null || nums.length == 0) return false;
         int i = 0;
         while (i < nums.length - 1) {
