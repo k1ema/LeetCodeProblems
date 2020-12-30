e9505b7f517bccec4d151b47c36276cebdb5ba73
klema <vklimen@gmail.com>
2020-12-30 12:33:07 +0200

    Added 289. Game of Life

---
+++ src/array/GameOfLife_289/SolutionTest.java
@@ -0,0 +1,21 @@
+package array.GameOfLife_289;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        int[][] actual = new int[][] {{0, 1, 0}, {0, 0, 1}, {1, 1, 1}, {0, 0, 0}};
+        int[][] expected = new int[][] {{0, 0, 0}, {1, 0, 1}, {0, 1, 1}, {0, 1, 0}};
+        s.gameOfLife(actual);
+        assertArrayEquals(expected, actual);
+
+        actual = new int[][] {{1, 1}, {1, 0}};
+        expected = new int[][] {{1, 1}, {1, 1}};
+        s.gameOfLife(actual);
+        assertArrayEquals(expected, actual);
+    }
+}
