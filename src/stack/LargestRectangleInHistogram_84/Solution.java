6f118745499dd799c5a65feef787d993f9553eaf
klema <vklimen@gmail.com>
2020-12-31 19:02:17 +0200

    Updated 84. Largest Rectangle in Histogram

--- src/stack/LargestRectangleInHistogram_84/Solution.java
+++ src/stack/LargestRectangleInHistogram_84/Solution.java
@@ -1,6 +1,7 @@
 package stack.LargestRectangleInHistogram_84;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * 84. Largest Rectangle in Histogram
@@ -29,25 +30,61 @@ import java.util.Stack;
 public class Solution {
     /*
         1. initialize stack and push -1
-        2. iterate heights and if stack contains other heights and cur height <= stack.pop:
-            res = max(res, heights[stack.pop] * (i - stack.peek() - 1))
-        3. push to stack anyway
-        4. after cycle check values in stack (while stack.peek != -1):
+        2. iterate heights
+            if stack contains other heights and cur height <= stack.peek:
+                res = max(res, heights[stack.pop] * (i - stack.peek() - 1))
+            push i to stack
+        3. after cycle check values in stack (while stack.peek != -1):
             res = max(res, heights[stack.pop] * (heights.length - stack.peek() - 1))
     */
     // tc O(n), sc O(n)
     public int largestRectangleArea(int[] heights) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);
+        if (heights == null || heights.length == 0) return 0;
+        int n = heights.length;
         int res = 0;
-        for (int i = 0; i < heights.length; i++) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1);
+        for (int i = 0; i < n; i++) {
             while (stack.peek() != -1 && heights[i] <= heights[stack.peek()]) {
-                res = Math.max(res, heights[stack.pop()] * (i - stack.peek() - 1));
+                res = Math.max(res, heights[stack.poll()] * (i - stack.peek() - 1));
             }
             stack.push(i);
         }
         while (stack.peek() != -1) {
-            res = Math.max(res, heights[stack.pop()] * (heights.length - stack.peek() - 1));
+            res = Math.max(res, heights[stack.poll()] * (n - stack.peek() - 1));
+        }
+        return res;
+    }
+
+    // divide and conquer
+    // tc: Average Case: O(nlogn), Worst Case: O(n^2)
+    // If the numbers in the array are sorted, we don't gain the advantage of divide and conquer.
+    // sc O(n)
+    public int largestRectangleArea1(int[] heights) {
+        return calculateArea(heights, 0, heights.length - 1);
+    }
+
+    public int calculateArea(int[] heights, int start, int end) {
+        if (start > end) return 0;
+        int minindex = start;
+        for (int i = start; i <= end; i++)
+            if (heights[minindex] > heights[i])
+                minindex = i;
+        return Math.max(heights[minindex] * (end - start + 1),
+                Math.max(calculateArea(heights, start, minindex - 1), calculateArea(heights, minindex + 1, end)));
+    }
+
+    // brute-force
+    // tc O(n^2), sc O(1)
+    public int largestRectangleArea2(int[] heights) {
+        int res = 0;
+        int n = heights.length;
+        for (int i = 0; i < n; i++) {
+            int minHeight = Integer.MAX_VALUE;
+            for (int j = i; j < n; j++) {
+                minHeight = Math.min(minHeight, heights[j]);
+                res = Math.max(res, minHeight * (j - i + 1));
+            }
         }
         return res;
     }
--- src/stack/LargestRectangleInHistogram_84/SolutionTest.java
+++ src/stack/LargestRectangleInHistogram_84/SolutionTest.java
@@ -11,5 +11,6 @@ public class SolutionTest {
         assertEquals(10, s.largestRectangleArea(new int[] {2, 1, 5, 6, 2, 3}));
         assertEquals(15, s.largestRectangleArea(new int[] {2, 1, 5, 6, 7, 3}));
         assertEquals(24, s.largestRectangleArea(new int[] {2, 1, 5, 6, 2, 3, 8, 8, 8}));
+        assertEquals(15, s.largestRectangleArea(new int[] {0, 1, 2, 3, 4, 5, 1, 3, 5, 6, 4, 3}));
     }
 }
