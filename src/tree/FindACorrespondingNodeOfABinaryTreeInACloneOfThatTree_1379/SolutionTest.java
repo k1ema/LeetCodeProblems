3c3d2d65904f74fe79ed9409ac1f7ef292bcdda3
klema <vklimen@gmail.com>
2021-01-02 13:55:46 +0200

    Added 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree

---
+++ src/tree/FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree_1379/SolutionTest.java
@@ -0,0 +1,15 @@
+package tree.FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree_1379;
+
+import org.junit.Test;
+import tree.utils.Parser;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(Parser.stringToTreeNode("[3,6,19]"), s.getTargetCopy(Parser.stringToTreeNode("[7,4,3,null,null,6,19]"),
+                Parser.stringToTreeNode("[7,4,3,null,null,6,19]"), Parser.stringToTreeNode("[3,6,19]")));
+    }
+}
