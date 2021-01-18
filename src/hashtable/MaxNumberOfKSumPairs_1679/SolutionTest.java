ce9a5975a0c2d4bee82526353b033c5366a3e108
klema <vklimen@gmail.com>
2021-01-18 22:10:17 +0200

    Added 1679. Max Number of K-Sum Pairs

---
+++ src/hashtable/MaxNumberOfKSumPairs_1679/SolutionTest.java
@@ -0,0 +1,15 @@
+package hashtable.MaxNumberOfKSumPairs_1679;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(2, s.maxOperations(new int[] {1, 2, 3, 4}, 5));
+        assertEquals(1, s.maxOperations(new int[] {3, 1, 3, 4, 3}, 6));
+        assertEquals(4, s.maxOperations(new int[] {2, 5, 4, 4, 1, 3, 4, 4, 1, 4, 4, 1, 2, 1, 2, 2, 3, 2, 4, 2}, 3));
+    }
+}
