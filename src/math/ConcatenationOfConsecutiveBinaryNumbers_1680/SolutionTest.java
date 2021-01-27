edb4b73966ad1a0dea251b8bdcb507b38e49cd20
klema <vklimen@gmail.com>
2021-01-27 22:12:20 +0200

    Added 1680. Concatenation of Consecutive Binary Numbers

---
+++ src/math/ConcatenationOfConsecutiveBinaryNumbers_1680/SolutionTest.java
@@ -0,0 +1,15 @@
+package math.ConcatenationOfConsecutiveBinaryNumbers_1680;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(1, s.concatenatedBinary(1));
+        assertEquals(27, s.concatenatedBinary(3));
+        assertEquals(505379714, s.concatenatedBinary(12));
+    }
+}
