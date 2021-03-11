ecfbaf544f52822f6f9007dca5bdb42537553933
klema <vklimen@gmail.com>
2021-03-11 12:25:25 +0200

    Updated 322. Coin Change

--- src/dynamic/CoinChange_322/Solution.java
+++ src/dynamic/CoinChange_322/Solution.java
@@ -6,44 +6,77 @@ import java.util.Arrays;
  * 322. Coin Change
  * https://leetcode.com/problems/coin-change/
  *
- * You are given coins of different denominations and a total amount of money amount. Write a function
- * to compute the fewest number of coins that you need to make up that amount. If that amount of money
- * cannot be made up by any combination of the coins, return -1.
+ * You are given coins of different denominations and a total amount of money amount. Write a function to
+ * compute the fewest number of coins that you need to make up that amount. If that amount of money cannot
+ * be made up by any combination of the coins, return -1.
+ *
+ * You may assume that you have an infinite number of each kind of coin.
  *
  * Example 1:
- * Input: coins = [1, 2, 5], amount = 11
+ * Input: coins = [1,2,5], amount = 11
  * Output: 3
  * Explanation: 11 = 5 + 5 + 1
  *
  * Example 2:
  * Input: coins = [2], amount = 3
  * Output: -1
- * Note:
- * You may assume that you have an infinite number of each kind of coin.
+ *
+ * Example 3:
+ * Input: coins = [1], amount = 0
+ * Output: 0
+ *
+ * Example 4:
+ * Input: coins = [1], amount = 1
+ * Output: 1
+ *
+ * Example 5:
+ * Input: coins = [1], amount = 2
+ * Output: 2
+ *
+ * Constraints:
+ * 1 <= coins.length <= 12
+ * 1 <= coins[i] <= 23^1 - 1
+ * 0 <= amount <= 10^4
  */
 public class Solution {
-    // tc O(n * amount), sc O(amount)
-    // 8 ms, faster than 92.61%; 35.7 MB, less than 98.82%
-    // https://leetcode.com/problems/coin-change/discuss/77360/c-onamount-time-oamount-space-dp-solution
-    int coinChange(int[] coins, int amount) {
-        // dp[i] is holding the min coins that consist number i
+    // tc O(n * amount), sc O(amount), bottom-up
+    /*
+           0 1 2 3 4 5 6 7 8 9 10 11
+        1  0 1 2 3 4 5 6 7 8 9 10 11
+        2  0 1 1 2 2 3 3 4 4 5  5  6
+        5  0 1 1 2 2 1 2
+    */
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) return 0;
+        Arrays.sort(coins);
         int[] dp = new int[amount + 1];
-        // set all values to amount + 1 as impossible value
-        Arrays.fill(dp, amount + 1);
-        // no money - no coin
+        Arrays.fill(dp, 10001);
         dp[0] = 0;
-        // for example, coins = {1, 2, 5}, amount = 11
-        // in the 1st round of iteration, coin is 1. so dp[i] = i.
-        // in the 2nd round dp[2] will be replaced from 2 '1's to 1 '2's; dp[4]: from 4 '1's to 2 '2's etc.
-        // in the 5th round dp[5] will be replaced from 3 (as 2 + 2 + 1) to 1.
         for (int i = 0; i < coins.length; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (j - coins[i] >= 0) {
-                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
-                }
+            for (int j = coins[i]; j <= amount; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
             }
         }
-        // dp[amount] is the min coins for consisting the amount
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amount] == 10001 ? -1 : dp[amount];
+    }
+
+    // tc O(n * amount), sc O(amount), top-down
+    public int coinChange1(int[] coins, int amount) {
+        Arrays.sort(coins);
+        int res = helper(coins, amount, new Integer[amount + 1]);
+        return res == 10001 ? -1 : res;
+    }
+
+    private int helper(int[] coins, int amount, Integer[] memo) {
+        if (amount == 0) return 0;
+        if (memo[amount] != null) return memo[amount];
+        int count = 10001;
+        for (int i = coins.length - 1; i >= 0; i--) {
+            if (amount - coins[i] >= 0) {
+                count = Math.min(count, 1 + helper(coins, amount - coins[i], memo));
+            }
+        }
+        memo[amount] = count;
+        return count;
     }
 }
--- src/dynamic/CoinChange_322/SolutionTest.java
+++ src/dynamic/CoinChange_322/SolutionTest.java
@@ -9,7 +9,12 @@ public class SolutionTest {
     public void test() {
         Solution s = new Solution();
         assertEquals(3, s.coinChange(new int[] {1, 2, 5}, 11));
+        assertEquals(-1, s.coinChange(new int[] {2}, 3));
+        assertEquals(0, s.coinChange(new int[] {1}, 0));
+        assertEquals(1, s.coinChange(new int[] {1}, 1));
+        assertEquals(2, s.coinChange(new int[] {1}, 2));
         assertEquals(3, s.coinChange(new int[] {2, 3}, 7));
         assertEquals(4, s.coinChange(new int[] {2, 3}, 10));
+        assertEquals(20, s.coinChange(new int[] {186, 419, 83, 408}, 6249));
     }
 }
