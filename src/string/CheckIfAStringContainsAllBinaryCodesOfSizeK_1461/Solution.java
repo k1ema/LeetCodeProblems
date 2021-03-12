ffa9d0f1231e7a2ea33c67c8bca4b5fa196d01ae
klema <vklimen@gmail.com>
2021-03-12 13:03:24 +0200

    Updated 1461. Check If a String Contains All Binary Codes of Size K

--- src/string/CheckIfAStringContainsAllBinaryCodesOfSizeK_1461/Solution.java
+++ src/string/CheckIfAStringContainsAllBinaryCodesOfSizeK_1461/Solution.java
@@ -41,7 +41,9 @@ import java.util.Set;
  * 1 <= k <= 20
  */
 public class Solution {
+    // tc O(n*k), sc O(2^k)
     public boolean hasAllCodes(String s, int k) {
+        if (s.length() < (1 << k)) return false;
         Set<String> set = new HashSet<>();
         for (int i = k; i <= s.length(); i++) {
             set.add(s.substring(i - k, i));
