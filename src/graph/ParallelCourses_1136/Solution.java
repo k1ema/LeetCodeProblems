b1085f9e2407dd3065048c412ca4cc8e6e948bf5
klema <vklimen@gmail.com>
2021-03-30 11:30:48 +0300

    Updated 1136. Parallel Courses

--- src/graph/ParallelCourses_1136/Solution.java
+++ src/graph/ParallelCourses_1136/Solution.java
@@ -1,7 +1,10 @@
 package graph.ParallelCourses_1136;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.stream.IntStream;
 
 /**
@@ -38,42 +41,92 @@ import java.util.stream.IntStream;
  * There are no repeated relations in the input.
  */
 public class Solution {
-    // topological sort, tc O(V + E), sc O(n), where n - the longest path
-    // 57 ms, faster than 9.92%, 44.5 MB, less than 100.00%
-    int minimumSemesters(int N, int[][] relations) {
-        List<Integer>[] graph = buildGraph(N, relations);
-        int[] depth = new int[N + 1];
+    // bfs, tc O(V + E), sc O(V + E)
+    // 5 ms, faster than 95.13%; 40.3 MB, less than 74.18%
+    public int minimumSemesters(int n, int[][] relations) {
+        List<Integer>[] graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        int[] indegree = new int[n + 1];
+        for (int[] rel : relations) {
+            graph[rel[0]].add(rel[1]);
+            indegree[rel[1]]++;
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+                indegree[i] = -1;
+                visited[i] = true;
+            }
+        }
+
+        int semesters = 0, visitedCount = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            semesters++;
+            for (int i = 0; i < size; i++) {
+                int u = q.poll();
+                visitedCount++;
+                for (int v : graph[u]) {
+                    if (!visited[v]) {
+                        indegree[v]--;
+                        if (indegree[v] == 0) {
+                            q.add(v);
+                            indegree[v] = -1;
+                            visited[v] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return semesters == 0 || visitedCount != n ? -1 : semesters;
+    }
+
+    // dfs, topological sort, tc O(V + E), sc O(n), where n - the longest path
+    // 6 ms, faster than 78.13%; 40.8 MB, less than 63.37%
+    public int minimumSemesters1(int n, int[][] relations) {
+        List<Integer>[] graph = buildGraph(n, relations);
+        int[] depth = new int[n + 1];
+        int[] state = new int[n + 1];
         for (int i = 1; i < graph.length; i++) {
-            if (!dfs(graph, new boolean[N + 1], new boolean[N + 1], i, depth)) return -1;
+            if (state[i] == 0) {
+                if (!dfs(graph, state, i, depth)) {
+                    return -1;
+                }
+            }
         }
         return IntStream.of(depth).max().orElse(-1);
     }
 
-    private boolean dfs(List<Integer>[] graph, boolean[] visited, boolean[] marked, int ind, int[] depth) {
-        if (marked[ind]) return false; // found cycle
-        if (visited[ind]) return true;
+    private boolean dfs(List<Integer>[] graph, int[] state, int u, int[] depth) {
+        if (state[u] == 1) return false; // found cycle
 
-        visited[ind] = true;
-        marked[ind] = true;
-
-        for (int j : graph[ind]) {
-            if (!dfs(graph, visited, marked, j, depth)) return false;
-            depth[ind] = Math.max(depth[ind], 1 + depth[j]);
+        state[u] = 1;
+        for (int v : graph[u]) {
+            if (state[v] != 2 && !dfs(graph, state, v, depth)) {
+                return false;
+            }
+            depth[u] = Math.max(depth[u], 1 + depth[v]);
         }
 
-        marked[ind] = false;
-        depth[ind] = Math.max(depth[ind], 1);
+        state[u] = 2;
+        depth[u] = Math.max(depth[u], 1);
         return true;
     }
 
-    private List<Integer>[] buildGraph(int N, int[][] relations) {
-        List<Integer>[] res = new LinkedList[N + 1];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = new LinkedList<>();
+    private List<Integer>[] buildGraph(int n, int[][] relations) {
+        List<Integer>[] g = new LinkedList[n + 1];
+        for (int i = 1; i < g.length; i++) {
+            g[i] = new LinkedList<>();
         }
-        for (int i = 0; i < relations.length; i++) {
-            res[relations[i][0]].add(relations[i][1]);
+        for (int[] rel : relations) {
+            g[rel[0]].add(rel[1]);
         }
-        return res;
+        return g;
     }
 }
