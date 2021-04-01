30444411af3b900ae32c86d1fc602e4adf51b685
klema <vklimen@gmail.com>
2021-04-01 21:55:35 +0300

    Updated 234. Palindrome Linked List

--- src/linkedList/PalindromeLinkedList_234/Solution.java
+++ src/linkedList/PalindromeLinkedList_234/Solution.java
@@ -4,6 +4,7 @@ import linkedList.utils.ListNode;
 
 /**
  * 234. Palindrome Linked List
+ * https://leetcode.com/problems/palindrome-linked-list/
  *
  * Given a singly linked list, determine if it is a palindrome.
  *
@@ -14,6 +15,11 @@ import linkedList.utils.ListNode;
  * Example 2:
  * Input: 1->2->2->1
  * Output: true
+ *
+ * Constraints:
+ * The number of nodes in the list is in the range [1, 105].
+ * 0 <= Node.val <= 9
+ *
  * Follow up:
  * Could you do it in O(n) time and O(1) space?
  */
@@ -22,36 +28,31 @@ public class Solution {
     public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) return true;
 
-        // find midlle
-        ListNode slow = null, fast = head;
-        ListNode l1 = head;
-        boolean even = false;
+        // find middle node
+        ListNode slow = head, fast = head;
+        int n = 0;
         while (fast != null && fast.next != null) {
-            slow = slow == null ? head : slow.next;
+            slow = slow.next;
             fast = fast.next.next;
+            n++;
         }
-        if (fast == null) even = true;
-        ListNode l2 = slow.next;
-        slow.next = null;
 
-        // reverse l1: prev.next = null, cur.next = prev
-        ListNode prev = null, cur = l1;
+        // reverse right half
+        ListNode prev = null, cur = slow;
         while (cur != null) {
             ListNode next = cur.next;
             cur.next = prev;
             prev = cur;
             cur = next;
         }
-        l1 = prev;
 
-        // compare l1 and l2
-        if (!even) l2 = l2.next;
-        while (l1 != null && l2 != null) {
-            if (l1.val != l2.val) return false;
-            l1 = l1.next;
-            l2 = l2.next;
+        // compare two parts
+        ListNode left = head, right = prev;
+        while (n-- > 0) {
+            if (left.val != right.val) return false;
+            left = left.next;
+            right = right.next;
         }
-
-        return l1 == null && l2 == null;
+        return true;
     }
 }
