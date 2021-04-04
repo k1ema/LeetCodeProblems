f744d0b2cdc3cd47d1f515ce1d63fefe4762a6dc
klema <vklimen@gmail.com>
2021-04-04 17:12:53 +0300

    Updated tests in 300. Longest Increasing Subsequence

--- src/dynamic/LongestIncreasingSubsequence_300/SolutionTest.java
+++ src/dynamic/LongestIncreasingSubsequence_300/SolutionTest.java
@@ -8,6 +8,7 @@ public class SolutionTest {
     @Test
     public void test() {
         Solution s = new Solution();
+        assertEquals(4, s.lengthOfLIS(new int[] {1, 10, 11, 2, 3, 4}));
         assertEquals(3, s.lengthOfLIS(new int[] {0, 8, 4, 12, 2}));
         assertEquals(4, s.lengthOfLIS(new int[] {10, 9, 2, 5, 3, 7, 101, 18}));
         assertEquals(4, s.lengthOfLIS(new int[] {1, 2, 3, 1, 1, 1, 4}));
