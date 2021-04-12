b1322edf2771140a1365baa609d0fe1cffba7a4b
klema <vklimen@gmail.com>
2021-04-12 14:26:06 +0300

    Updated 84. Largest Rectangle in Histogram: added segment tree solution

--- src/stack/LargestRectangleInHistogram_84/Solution.java
+++ src/stack/LargestRectangleInHistogram_84/Solution.java
@@ -38,7 +38,7 @@ public class Solution {
             res = max(res, heights[stack.pop] * (heights.length - stack.peek() - 1))
     */
     // tc O(n), sc O(n)
-    public int largestRectangleArea(int[] heights) {
+    public int largestRectangleArea2(int[] heights) {
         if (heights == null || heights.length == 0) return 0;
         int n = heights.length;
         int res = 0;
@@ -74,9 +74,79 @@ public class Solution {
                 Math.max(calculateArea(heights, start, minindex - 1), calculateArea(heights, minindex + 1, end)));
     }
 
+    // segment tree
+    // tc O(nlogn), sc O(n)
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        SegmentTree st = new SegmentTree(heights);
+        return calculateArea(heights, 0, n - 1, st);
+    }
+
+    public int calculateArea(int[] heights, int start, int end, SegmentTree st) {
+        if (start > end) return 0;
+        if (start == end) return heights[start];
+        int minInd = st.getMin(start, end);
+        int area1 = heights[minInd] * (end - start + 1);
+        int area2 = calculateArea(heights, start, minInd - 1, st);
+        int area3 = calculateArea(heights, minInd + 1, end, st);
+        return Math.max(area1, Math.max(area2, area3));
+    }
+
+    private static class SegmentTree {
+        private static final int INF = (int) 1e9;
+        private int n, offset;
+        private int[][] nums;
+
+        SegmentTree(int[] arr) {
+            int len = arr.length;
+            int k = 0;
+            while ((1 << k) < len) {
+                k++;
+            }
+            n = 1 << k;
+            this.nums = new int[2 * n - 1][2];
+            this.offset = nums.length - n;
+            for (int i = offset; i < nums.length; i++) {
+                nums[i] = new int[] {i - offset < arr.length ? arr[i - offset] : INF, i - offset};
+            }
+            for (int i = 0; i < n; i += 2) {
+                update(i, nums[offset + i]);
+            }
+        }
+
+        int getMin(int L, int R) {
+            return getMin(0, 0, n - 1, L, R)[1];
+        }
+
+        int[] getMin(int i, int l, int r, int L, int R) {
+            if (r < L || l > R) return new int[] {INF, -1};
+            if (l >= L && r <= R) return nums[i];
+            int m = l + (r - l) / 2;
+            int[] leftChild = getMin(2 * i + 1, l, m, L, R);
+            int[] rightChild = getMin(2 * i + 2, m + 1, r, L, R);
+            return leftChild[0] < rightChild[0] ? leftChild : rightChild;
+        }
+
+        void update(int i, int[] val) {
+            int ind = offset + i;
+            nums[ind] = val;
+            while (ind > 0) {
+                int parentInd = (ind - 1) / 2;
+                int leftChildInd = 2 * parentInd + 1;
+                int rightChildInd = 2 * parentInd + 2;
+                if (nums[leftChildInd][0] < nums[rightChildInd][0]) {
+                    nums[parentInd] = nums[leftChildInd];
+                } else {
+                    nums[parentInd] = nums[rightChildInd];
+                }
+                ind = (ind - 1) / 2;
+            }
+        }
+    }
+
     // brute-force
     // tc O(n^2), sc O(1)
-    public int largestRectangleArea2(int[] heights) {
+    public int largestRectangleArea3(int[] heights) {
         int res = 0;
         int n = heights.length;
         for (int i = 0; i < n; i++) {
