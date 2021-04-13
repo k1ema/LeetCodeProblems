ee512f31f3477ef68dfee13414e2ae25f73431d4
klema <vklimen@gmail.com>
2021-04-13 18:23:22 +0300

    Updated 42. Trapping Rain Water

--- src/dynamic/TrappingRainWater_42/Solution.java
+++ src/dynamic/TrappingRainWater_42/Solution.java
@@ -25,20 +25,19 @@ public class Solution {
     // 1 ms, faster than 94.21%; 39.2 MB, less than 27.40%
     public int trap(int[] height) {
         int n = height.length;
-        if (n == 0) return 0;
         int[] leftMax = new int[n];
         int[] rightMax = new int[n];
-        leftMax[0] = height[0];
-        rightMax[n - 1] = height[n - 1];
-        for (int i = 1; i < n; i++) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        for (int i = 0; i < height.length; i++) {
+            leftMax[i] = i == 0 ? height[i] : Math.max(height[i], leftMax[i - 1]);
         }
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        for (int i = n - 1; i >= 0; i--) {
+            rightMax[i] = i == n - 1 ? height[i] : Math.max(height[i], rightMax[i + 1]);
         }
         int res = 0;
-        for (int i = 0; i < n; i++) {
-            res += Math.min(leftMax[i], rightMax[i]) - height[i];
+        for (int i = 1; i < n - 1; i++) {
+            if (height[i] < leftMax[i - 1] && height[i] < rightMax[i + 1]) {
+                res += Math.min(leftMax[i - 1], rightMax[i + 1]) - height[i];
+            }
         }
         return res;
     }
